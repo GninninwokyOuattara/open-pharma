0ce24d31f9c9bcc05af07e0ddd73aa449b77bb65
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-11-24 17:43:31 +0000

    current change saved

---
+++ charbon/ubifarm.py
@@ -0,0 +1,89 @@
+import asyncio
+from datetime import datetime
+import os
+import json
+
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from requests import request
+from constants.selectors import PHARMACY_DIRECTOR, PHARMACY_TITLE, UBIFARM_PHARMACY
+
+from constants.selectors import UBIFARM_NUMPAGE
+from constants.links import UBIFARM_URL
+
+
+# loop = asyncio.get_event_loop()
+
+async def asyncFetch(url : str):
+    return request(method="GET", url=url)
+
+
+async def pageScrap(url : str, pageNumber = 1):
+    page = request(method="GET", url=f"{url}?pageannu={pageNumber}")
+    soup = BeautifulSoup(page.text, "html.parser")
+    
+    # Retrieve all pharmacies within the page
+    # datas = soup.select(UBIFARM_PHARMACY)
+    datas = soup.select('li[data-country="COTE D\'IVOIRE"]')
+    pharmacies = []
+    for data in datas:
+        pharma_title = data.select_one(PHARMACY_TITLE).getText(strip=True).replace("\u00c8", "").replace("*", "")
+        pharma_director = data.select_one(PHARMACY_DIRECTOR).text.replace("\t", "").replace("\r", "")[1:-1].split("\n")[0]
+        pharmacies.append({"pharmacy_name" : pharma_title, "pharmacy_director" : pharma_director})
+    return pharmacies
+
+# async def main():
+
+#     tasks = []
+#     for i in range(2):
+#         tasks.append(asyncio.create_task(asyncFetch(UBIFARM_URL)))
+#     e = await asyncio.gather(*tasks)
+#     print(e)
+
+# results = asyncio.run(main())
+
+
+# print("Getting the maximum number of pages to scrap...")
+
+page = request(method="GET", url=UBIFARM_URL)
+pageSoup = BeautifulSoup(page.text, "html.parser")
+numPage = pageSoup.select_one(UBIFARM_NUMPAGE).text.split("/")[1].strip()
+
+
+async def mainScraper():
+    tasks  = []
+    #Run pageScrap() for each page
+    print("Building targeted address links")
+    pbar = tqdm(range(int(numPage)+1))
+    for i in pbar:
+        pbar.set_description(f"Target {i} - {UBIFARM_URL}?pageannu={i+1}")
+        tasks.append(asyncio.create_task(pageScrap(UBIFARM_URL, pageNumber=i+1)))
+    results = await asyncio.gather(*tasks)
+    # Results contains a tuple of array of element returned by pageScrap
+    # We need to make it into one array
+    endResults = []
+    
+    for element in results:
+        print(f"Start scraping for page : {i}", end="", flush=True)
+        endResults = [*endResults, *element]
+    print("\nScraping completed.")
+    return endResults
+    
+    
+collectedDatas = asyncio.run(mainScraper())
+
+
+if not os.path.exists("./datas"):
+    os.mkdir("./datas")
+
+
+now = datetime.now()
+filename = now.strftime("%d-%m-%Y-%H-%M-%S")
+
+print(f"Saving collected datas in /datas/{filename}.json", end=" ")
+
+with open(f"./datas/{filename}.json", "w+") as f:
+    collectedDatas = json.dumps(collectedDatas)
+    f.write(collectedDatas)
+
+print("DONE.")
