b8c63578cdf9e048644eff285cc71d96a9fafbaa
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-11-25 15:52:07 +0000

    choose to ignore data that are too unspecific for google maps, they will be processed via selenium

--- charbon/main.py
+++ charbon/main.py
@@ -4,13 +4,16 @@ import asyncio
 import urllib
 import os
 import json
+import re
 
 from datetime import datetime
 from requests import request
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-from constants.selectors import PHARMACY_DIRECTOR, PHARMACY_TITLE, UBIFARM_NUMPAGE, UBIFARM_PHARMACY
+from selenium import webdriver
 
+
+from constants.selectors import PHARMACY_DIRECTOR, PHARMACY_TITLE, UBIFARM_NUMPAGE, UBIFARM_PHARMACY
 from constants.links import UBIFARM_URL
 
 
@@ -22,6 +25,7 @@ class WebScraper:
         self.urls = []
         self.tqdm : tqdm = None
         self.all_datas = []
+        self.selenium_bin = []
         self.page_max_number = self.getPageMaxNumber()
         
         asyncio.run(self.main())
@@ -40,14 +44,35 @@ class WebScraper:
             return pharmacies
         
     async def fetchCoordinates(self, session, data):
+        """Fetch google maps coordinates for an address
+        data is the format {pharmacy_name : str, pharmacy_director : str}"""
         name = data["pharmacy_name"]
         url = f"https://google.com/maps/search/{urllib.parse.quote(name)}"
         async with session.get(url) as response:
             res = await response.text()
             soup = BeautifulSoup(res, "html.parser")
-            meta = str(soup.select_one('meta[content^="https://"]'))
-            coordinates = meta[65:meta.index("&amp")].split("%2C")
-            data["coordinates"] = { "lat" : float(coordinates[0]), "lng" : float(coordinates[1])}
+            # sometimes when the search is not unique enough for google maps it will return all element in proximity
+            proxim = soup.select_one('meta[content^="Pharmacie à proximité"]')
+            if proxim:
+                #In case we do not get a precise result, we insert in into array that will be processed using selenium, google maps use javascript.
+                self.selenium_bin.append(data)
+                
+                # To delete later
+                # link = str(soup.select_one('a[href^="https://www.google.com/maps"]').href)
+                # lat,lng = re.findall(r'\d[.]\d*', link)
+                # data["coordinates"] = {"lat" : lat, "lng" : lng}
+                return []
+            
+            else:
+                try:
+                    meta = str(soup.select_one('meta[content^="https://"]'))
+                    coordinates = meta[65:meta.index("&amp")].split("%2C")
+                    data["coordinates"] = { "lat" : float(coordinates[0]), "lng" : float(coordinates[1])}
+                except ValueError as e:
+                    print(data)
+                    print(str(e))
+                    raise e
+                
             self.tqdm.update(1)
             return data
             
@@ -63,7 +88,7 @@ class WebScraper:
         return pharmacies
     
     async def main(self):
-        print("Fetching all pharmacies....")
+        print("Fetching all pharmacies....\n")
         headers = {
             "user-agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
         async with aiohttp.ClientSession(headers = headers) as session:
@@ -76,19 +101,22 @@ class WebScraper:
             
             for element in results:
                 self.all_datas = [*self.all_datas, *element]
-            # print("DONE")
         
-        print("Fetching pharmacies maps coordinates...")
+        print("Fetching pharmacies maps coordinates...\n")
         async with aiohttp.ClientSession(headers = headers) as session:
             tasks = [self.fetchCoordinates(session, data=data) for data in self.all_datas]
             self.tqdm = tqdm(total=len(self.all_datas))
             results = await asyncio.gather(*tasks)
             self.tqdm.close()
-            print("Done for the day")
             self.all_datas = []
             
             for element in results:
                 self.all_datas = [*self.all_datas, element]
+            
+            print("To be seleniumed", len(self.selenium_bin))
+            
+            # Implement the selenium counterpart later
+            
         return 
     
 scraper = WebScraper()
