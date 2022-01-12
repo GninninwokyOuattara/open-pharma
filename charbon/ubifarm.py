166d5d91867123178e1088394eb3963a6558aceb
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-01-12 00:08:30 +0000

    current advancement, looking for a way to match data to their zone

--- charbon/ubifarm.py
+++ charbon/ubifarm.py
@@ -4,106 +4,98 @@ import os
 import json
 
 from bs4 import BeautifulSoup
+import re
+from bs4.element import Tag
 from tqdm import tqdm
-from requests import request
-import requests_async as asyncReq
+import requests
 from constants.selectors import PHARMACY_DIRECTOR, PHARMACY_TITLE, UBIFARM_PHARMACY
 
-from constants.selectors import UBIFARM_NUMPAGE
-from constants.links import UBIFARM_URL
-import random
+from constants.selectors import UBIFARM_NUMPAGE, PHARMA_TABLE, PHARMA_LOCATION
+from constants.links import  PHARMA_CONSULT_URL
 
-# loop = asyncio.get_event_loop()
 
-async def asyncFetch(url : str):
-    return request(method="GET", url=url)
-
-async def extractPharmacies(page : str):
-    soup = BeautifulSoup(page.text, "html.parser")
+class Ubifarm:
     
-    #Retrieve all pharmacies within the page
-    datas = soup.select(UBIFARM_PHARMACY)
-    datas = soup.select('li[data-country="COTE D\'IVOIRE"]')
-    pharmacies = []
-    for data in datas:
-        pharma_title = data.select_one(PHARMACY_TITLE).getText(strip=True).replace("\u00c8", "").replace("*", "")
-        pharma_director = data.select_one(PHARMACY_DIRECTOR).text.replace("\t", "").replace("\r", "")[1:-1].split("\n")[0]
-        pharmacies.append({"pharmacy_name" : pharma_title, "pharmacy_director" : pharma_director})
-    return pharmacies
-
-
-async def pageScrap(url : str, pageNumber = 1, pbar  : tqdm = None):
-    await asyncReq.get(f"{url}?pageannu={pageNumber}")
-    page = request(method="GET", url=f"{url}?pageannu={pageNumber}")
-    # soup = BeautifulSoup(page.text, "html.parser")
+    def __init__(self, pharmaConsult : str):
+        self.ubifarmUrl = pharmaConsult
+        self.pageSoup : BeautifulSoup = self.getPage() 
+        # self.page = requests.get(url=ubifarmUrl)
+        
     
-    # Retrieve all pharmacies within the page
-    # datas = soup.select(UBIFARM_PHARMACY)
-    # datas = soup.select('li[data-country="COTE D\'IVOIRE"]')
-    pharmacies = await extractPharmacies(page)
-    # for data in datas:
-    #     pharma_title = data.select_one(PHARMACY_TITLE).getText(strip=True).replace("\u00c8", "").replace("*", "")
-    #     pharma_director = data.select_one(PHARMACY_DIRECTOR).text.replace("\t", "").replace("\r", "")[1:-1].split("\n")[0]
-    #     pharmacies.append({"pharmacy_name" : pharma_title, "pharmacy_director" : pharma_director})
-
-    if pbar:
-        pbar.update(1)
-    return pharmacies
-
-# async def main():
-
-#     tasks = []
-#     for i in range(2):
-#         tasks.append(asyncio.create_task(asyncFetch(UBIFARM_URL)))
-#     e = await asyncio.gather(*tasks)
-#     print(e)
-
-# results = asyncio.run(main())
-
-
-# print("Getting the maximum number of pages to scrap...")
-
-page = request(method="GET", url=UBIFARM_URL)
-pageSoup = BeautifulSoup(page.text, "html.parser")
-numPage = pageSoup.select_one(UBIFARM_NUMPAGE).text.split("/")[1].strip()
-
-
-async def mainScraper():
-    # tasks  = []
-    # #Run pageScrap() for each page
-    # print("Building targeted address links")
-    # pbar = tqdm(range(int(numPage)+1))
-    # for i in pbar:
-    #     pbar.set_description(f"Target {i} - {UBIFARM_URL}?pageannu={i+1}")
-    #     tasks.append(asyncio.create_task(pageScrap(UBIFARM_URL, pageNumber=i+1)))
-    tq = tqdm(total=int(numPage)+1)
-    tasks = [asyncio.create_task(pageScrap(UBIFARM_URL, pageNumber=i, pbar=tq)) for i in range(int(numPage)+1)]
-    results = await asyncio.gather(*tasks)
-    tq.close()
-    # Results contains a tuple of array of element returned by pageScrap
-    # We need to make it into one array
-    endResults = []
+    def getPage(self):
+        try:
+            res = requests.get(url=self.ubifarmUrl)
+            if not res.status_code == 200:
+                raise 
+            soup = BeautifulSoup(res.text, "html.parser")
+            # self.pageSoup = soup
+            return soup
+        except Exception as exc:
+            raise exc
     
-    for element in results:
-        endResults = [*endResults, *element]
-    print("\nScraping completed.")
-    return endResults
+    def getHeaders(self):
+        headers = self.pageSoup.select(PHARMA_LOCATION)
+        return headers
+
+    def getTablesDatas(self):
+        tables = self.pageSoup.select(PHARMA_TABLE)
+        return tables
+    
+    def getTableRowsData(self, table):
+        rows = table.select("tbody tr")
+        rowsData = []
+        for row in rows:
+            pharmacy = OpenPharmacy(row)
+            pharmacy = pharmacy.getObject()
+            rowsData.append(pharmacy)
+        return rowsData
+            # print(pharmacy)
+        
+        
+    def extractAllTable(self):
+        tables = self.pageSoup.select(PHARMA_TABLE)
+        result = []
+        for table in tables:
+            # rows = table.select("tbody tr")
+            # for row in rows:
+            #     pharmacy = OpenPharmacy(row)
+            #     pharmacy = pharmacy.getObject()
+            #     print(pharmacy)
+            rowsData = self.getTableRowsData(table)
+    
+    def matchingTableToHeaders(self):
+        pass
+        
+            
+        
+class OpenPharmacy:
+    
+    def __init__(self, row : Tag):
+        self.name = row.select("td")[0].text
+        self.owner = row.select("td")[1].text
+        self.contact = row.select("td")[2].text
+        self.localisation = row.select("td")[3].text
+        self.position = (row.select("td")[4].find("a"))
+        if self.position:
+            self.position = self.position["href"]
+            lat, lng = re.findall(r'\d[.]\d*', self.position)[0:2]
+            self.position = f"{lat},{lng}"
+        else:
+            self.position = ""
+        self.start = row.select("td")[5].text
+        self.end = row.select("td")[6].text
+    
+    def getObject(self):
+        return { "name" : self.name, "owner" : self.owner, "contact" : self.contact, "localisation" : self.localisation, "position" : self.position, "start" : self.start, "end" : self.end}
+            
+        
+    def __repr__(self) -> str:
+       string = "{" + f"name : {self.name}, owner : {self.owner}, contact : {self.contact}, localisation : {self.localisation}, position : {self.position}, start : {self.start}, end : {self.end}" + "}" 
+       return string
+
+    
+if __name__ == "__main__":
+    ubi = Ubifarm(PHARMA_CONSULT_URL)
+    ubi.extractAllTable()
     
     
-collectedDatas = asyncio.run(mainScraper())
-
-
-if not os.path.exists("./datas"):
-    os.mkdir("./datas")
-
-
-now = datetime.now()
-filename = now.strftime("%d-%m-%Y-%H-%M-%S")
-
-print(f"Saving collected datas in /datas/{filename}.json", end=" ")
-
-with open(f"./datas/{filename}.json", "w+") as f:
-    collectedDatas = json.dumps(collectedDatas)
-    f.write(collectedDatas)
-
-print("DONE.")
