48f3e189c6c3a16f298131b8f963456cd24547c1
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-02-07 09:24:56 +0000

    working

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
 import * as React from "react";
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import useAxios from "axios-hooks";
 
-import { PROJECT_ENDPOINT } from "@env";
+import { PROJECT_ENDPOINT, ALL_PHARMACIES } from "@env";
 
 export default function App() {
-    console.log(PROJECT_ENDPOINT);
+    console.log(PROJECT_ENDPOINT, ALL_PHARMACIES);
+    const [{ data, loading, error }, refetch] = useAxios(
+        { url: `${PROJECT_ENDPOINT}${ALL_PHARMACIES}.json`, method: "GET" },
+        {
+            manual: true,
+        }
+    );
+    React.useEffect(() => {
+        refetch();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!!</Text>
             <StatusBar style="auto" />
+            <Text>Open up App.tsx to start working on your app!!</Text>
+            <Button
+                onPress={() => refetch()}
+                title="Click Me"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
         </View>
     );
 }
