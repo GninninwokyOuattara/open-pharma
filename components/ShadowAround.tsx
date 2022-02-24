2b144aab409c6273248a770980404759ca0266cd
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-02-24 16:52:57 +0000

    Shadow component to easily a rapidly wrap put shadow around most view

---
+++ components/ShadowAround.tsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+interface shadowStyle {
+    shadowColor?: string;
+    shadowOffset?: {
+        width: number;
+        height: number;
+    };
+    shadowOpacity?: number;
+    shadowRadius?: number;
+    elevation?: number;
+}
+
+interface props {
+    children: React.ReactNode;
+    shadowStyles: shadowStyle;
+}
+
+const ShadowAround: React.FC<props> = ({ children, shadowStyles }) => {
+    return (
+        <View style={{ ...styles.container, ...shadowStyles }}>{children}</View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+
+        elevation: 9,
+    },
+});
+
+export default ShadowAround;
