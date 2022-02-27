74787dc67944c79500ee2474dfc3bb47615e04f1
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-02-27 00:05:30 +0000

    Add new case handling for UPDATE_RELATIVE_DISTANCES

--- stores/pharmaciesReducer.ts
+++ stores/pharmaciesReducer.ts
@@ -1,6 +1,10 @@
 import { PROJECT_ENDPOINT, ALL_PHARMACIES } from "@env";
 import { Pharmacy } from "../types/dataTypes";
-import { FETCH_OPEN_PHARMACIES, FETCH_ALL_PHARMACIES } from "./actions";
+import {
+    FETCH_OPEN_PHARMACIES,
+    FETCH_ALL_PHARMACIES,
+    UPDATE_RELATIVE_DISTANCES,
+} from "./actions";
 
 interface PharmaciesState {
     all: Pharmacy[];
@@ -16,6 +20,10 @@ export default (state: PharmaciesState = pharmaciesState, action: any) => {
     switch (action.type) {
         case FETCH_ALL_PHARMACIES:
             return { ...state, all: action.data };
+        case UPDATE_RELATIVE_DISTANCES:
+            console.log(action.data[1]);
+            const obj = { ...state, all: action.data };
+            return obj;
         default:
             return { ...state };
     }
