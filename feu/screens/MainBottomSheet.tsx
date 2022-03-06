75e30ea2dccf52b5270268921ef0ebe5a5408956
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-03-06 15:46:15 +0000

    BottomSheet can no longer go over statusBar

--- feu/screens/MainBottomSheet.tsx
+++ feu/screens/MainBottomSheet.tsx
@@ -1,17 +1,17 @@
 import React, { useCallback, useMemo, useRef } from "react";
 import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
-import usePharmaciesData from "../hooks/usePharmaciesData";
-import PharmaItem from "../components/PharmaItem";
 import { calculateDistance } from "../utils/calculateDistance";
 import BottomSheetContent from "./BottomSheetContent";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const MainBottomSheet = () => {
     // ref
     const bottomSheetRef = useRef<BottomSheet>(null);
 
     // variables
-    const snapPoints = useMemo(() => ["10%", "25%", "50%", "90%"], []);
+    const insets = useSafeAreaInsets();
+    const snapPoints = useMemo(() => [24, "50%", "100%"], []);
 
     // callbacks
     const handleSheetChanges = useCallback((index: number) => {
@@ -25,11 +25,8 @@ const MainBottomSheet = () => {
             index={1}
             snapPoints={snapPoints}
             onChange={handleSheetChanges}
-            handleStyle={{
-                backgroundColor: "orange",
-                borderTopLeftRadius: 15,
-                borderTopRightRadius: 15,
-            }}
+            topInset={insets.top}
+            backgroundStyle={{ backgroundColor: "#F0ECD6", opacity: 0.9 }}
         >
             <BottomSheetContent />
         </BottomSheet>
@@ -49,6 +46,16 @@ const styles = StyleSheet.create({
     bottomSheetContainer: {
         flex: 1,
         paddingHorizontal: 10,
+    },
+    textInput: {
+        alignSelf: "stretch",
+        marginHorizontal: 12,
+        marginBottom: 12,
+        padding: 12,
+        borderRadius: 12,
+        backgroundColor: "grey",
+        color: "white",
+        textAlign: "center",
     },
 });
 
