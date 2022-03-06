ac9397e6372679bead99a0a19488fe2d553f26b1
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-03-06 16:59:41 +0000

    wrapped renderFunction with usCallBack hook and added a custom searchBar to the bottomSheet

--- feu/screens/BottomSheetContent.tsx
+++ feu/screens/BottomSheetContent.tsx
@@ -1,37 +1,41 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useCallback } from "react";
 import usePharmaciesData from "../hooks/usePharmaciesData";
 import PharmaItem from "../components/PharmaItem";
 import { BottomSheetFlatList } from "@gorhom/bottom-sheet";
 import { Pharmacy } from "../types/dataTypes";
+import CustomSearchBar from "./CustomSearchBar";
 
 const BottomSheetContent = () => {
     const pharmaciesDatas = usePharmaciesData();
 
-    const renderPharmaciesItems = ({ item }: { item: Pharmacy }) => {
-        console.log("Rendered using renderer :)");
-        return (
-            <PharmaItem
-                key={item.Id}
-                imageUrl={
-                    "https://img.freepik.com/free-vector/pharmacy-building-isolated-white_180264-152.jpg?w=740"
-                }
-                data={{
-                    pharmacyName: item.Nom,
-                    pharmacyLocation: item.Localisation,
-                    distance: item.Distance,
-                }}
-            />
-        );
-    };
+    const renderPharmaciesItems = useCallback(
+        ({ item }: { item: Pharmacy }) => {
+            return (
+                <PharmaItem
+                    key={item.Id}
+                    imageUrl={
+                        "https://img.freepik.com/free-vector/pharmacy-building-isolated-white_180264-152.jpg?w=740"
+                    }
+                    data={{
+                        pharmacyName: item.Nom,
+                        pharmacyLocation: item.Localisation,
+                        distance: item.Distance,
+                    }}
+                />
+            );
+        },
+        [pharmaciesDatas]
+    );
 
     return (
         <BottomSheetFlatList
+            ListHeaderComponent={<CustomSearchBar />}
             data={pharmaciesDatas}
             keyExtractor={(item) => item.Id}
             renderItem={renderPharmaciesItems}
             contentContainerStyle={styles.contentContainer}
-        />
+        ></BottomSheetFlatList>
     );
 };
 
