cf512d7b978680395518651ce9a591d2c05f98b8
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-03-11 15:45:53 +0000

    forward location data to fetchLocalPharmaciesData hook

--- feu/hooks/usePharmaciesData.tsx
+++ feu/hooks/usePharmaciesData.tsx
@@ -3,19 +3,22 @@ import React, { useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootReducerType } from "../types/dataTypes";
 import { fetchLocalPharmaciesData } from "../stores/pharmaciesActions";
+import useLocation from "./useLocation";
 
 const usePharmaciesData = () => {
     const dispatch = useDispatch();
-    const pharmacies = useSelector((state: RootReducerType) => {
-        return state.pharmacies.all;
+    const { location, errorMsg } = useLocation();
+
+    const [all, toDisplay] = useSelector((state: RootReducerType) => {
+        return [state.pharmacies.all, state.pharmacies.toDisplay];
     });
 
     useEffect(() => {
         // dispatch(fetchAllPharmacies());
-        dispatch(fetchLocalPharmaciesData());
+        dispatch(fetchLocalPharmaciesData(location));
     }, [dispatch]);
 
-    return pharmacies;
+    return toDisplay;
 };
 
 export default usePharmaciesData;
