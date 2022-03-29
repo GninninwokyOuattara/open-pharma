90cce8c609e13ed5b315a6682f1717f6e0c7010f
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-03-29 15:12:03 +0000

    Add a state value, selectedMarker

--- feu/contexts/MapContext.tsx
+++ feu/contexts/MapContext.tsx
@@ -9,20 +9,31 @@ import MapView from "react-native-maps";
 
 export interface MapContextType {
     mapRef: RefObject<MapView> | null;
+    selectedMarker: string;
+    setSelectedMarker: React.Dispatch<React.SetStateAction<string>> | null;
 }
 
 interface MapProviderProps {
     children: JSX.Element | JSX.Element[];
 }
 
-export const MapContext = createContext<MapContextType>({ mapRef: null });
+export const MapContext = createContext<MapContextType>({
+    mapRef: null,
+    selectedMarker: "",
+    setSelectedMarker: null,
+});
 
 export const MapContextProvider: React.FC<MapProviderProps> = ({
     children,
 }) => {
     const mapRef = useRef<MapView>(null);
+    const [selectedMarker, setSelectedMarker] = useState("");
 
     return (
-        <MapContext.Provider value={{ mapRef }}>{children}</MapContext.Provider>
+        <MapContext.Provider
+            value={{ mapRef, selectedMarker, setSelectedMarker }}
+        >
+            {children}
+        </MapContext.Provider>
     );
 };
