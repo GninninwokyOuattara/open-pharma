cf02ecdc3d17c844bb65be0394d3d208249248f7
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-04-07 17:32:17 +0000

    Main component

---
+++ feu/Main.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import useAxios from "axios-hooks";
+
+import { PROJECT_ENDPOINT, ALL_PHARMACIES } from "@env";
+
+import Map from "./components/screens-components/Map";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+
+import { Provider, useDispatch } from "react-redux";
+import pharmaciesReducer from "./stores/pharmaciesReducer";
+import MainScreen from "./screens/MainScreen";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import * as Location from "expo-location";
+import AppLoading from "expo-app-loading";
+import LocationDeniedScreen from "./screens/LocationDeniedScreen";
+import { COLOR_SCHEME } from "./constants/colorSchemes";
+import {
+    fetchAllPharmacies,
+    fetchLocalPharmaciesData,
+} from "./stores/pharmaciesActions";
+
+const Main = () => {
+    const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
+    const [location, setLocation] =
+        React.useState<Location.LocationObject | null>(null);
+    const [isReady, setIsReady] = React.useState(false);
+
+    const dispatch = useDispatch();
+
+    const getLocationPermission = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            setErrorMsg("Permission to access location was denied");
+            setIsReady(true);
+
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        setIsReady(true);
+    };
+
+    // const rootReducers = combineReducers({
+    //     pharmacies: pharmaciesReducer,
+    // });
+
+    // const store = createStore(rootReducers, applyMiddleware(ReduxThunk));
+
+    React.useEffect(() => {
+        dispatch(fetchLocalPharmaciesData);
+        // (async () => {
+
+        //     getLocationPermission();
+        // })();
+    }, [dispatch]);
+
+    if (!isReady) {
+        return <AppLoading />;
+    } else {
+        if (!location) {
+            return (
+                <SafeAreaView
+                    style={{
+                        flex: 1,
+                        backgroundColor: COLOR_SCHEME.LIGHT_ORANGE,
+                    }}
+                >
+                    <StatusBar style="auto" />
+                    {/* <Provider store={store}> */}
+                    <LocationDeniedScreen />
+                    {/* </Provider> */}
+                </SafeAreaView>
+            );
+        }
+        return (
+            <SafeAreaProvider>
+                <StatusBar style="auto" />
+                {/* <Provider store={store}> */}
+                <MainScreen />
+                {/* </Provider> */}
+            </SafeAreaProvider>
+        );
+    }
+};
+
+export default Main;
