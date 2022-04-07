4e81f5758580639c2eb07c305e24bc3af4e83cb9
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-04-07 11:28:35 +0000

    Show map with screen only when user give access to his location

--- feu/App.tsx
+++ feu/App.tsx
@@ -16,32 +16,65 @@ import pharmaciesReducer from "./stores/pharmaciesReducer";
 import MainScreen from "./screens/MainScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import * as Location from "expo-location";
+import AppLoading from "expo-app-loading";
+
 export default function App() {
+    const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
+    const [location, setLocation] =
+        React.useState<Location.LocationObject | null>(null);
+    const [isReady, setIsReady] = React.useState(false);
+
+    const getLocationPermission = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            setErrorMsg("Permission to access location was denied");
+            setIsReady(true);
+
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        setIsReady(true);
+    };
+
     const rootReducers = combineReducers({
         pharmacies: pharmaciesReducer,
     });
 
     const store = createStore(rootReducers, applyMiddleware(ReduxThunk));
 
-    return (
-        <SafeAreaProvider>
-            <StatusBar style="auto" />
-            <Provider store={store}>
-                <MainScreen />
-            </Provider>
-        </SafeAreaProvider>
-        // <SafeAreaView style={styles.container}>
-        //     <StatusBar style="auto" />
-        //     <BottomSheetScreen />
-        // </SafeAreaView>
-    );
+    React.useEffect(() => {
+        getLocationPermission();
+    }, []);
+
+    if (!isReady) {
+        return <AppLoading />;
+    } else {
+        if (!location) {
+            return (
+                <View style={styles.container}>
+                    <Text>Location Permission not provided</Text>
+                </View>
+            );
+        }
+        return (
+            <SafeAreaProvider>
+                <StatusBar style="auto" />
+                <Provider store={store}>
+                    <MainScreen />
+                </Provider>
+            </SafeAreaProvider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fee",
-        // alignItems: "center",
-        // justifyContent: "center",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
