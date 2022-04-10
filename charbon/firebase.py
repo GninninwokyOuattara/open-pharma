41e636d5c2ff0df678d892276750c6b5ccd17a79
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2022-04-10 02:06:35 +0000

    retry

--- charbon/firebase.py
+++ charbon/firebase.py
@@ -1,14 +1,15 @@
 #DO NOT COMMIT THIS UNTIL YOU HAVE PROPER ENVIRONNEMENT SETUP
 from os import environ
+from ast import literal_eval
 from firebase_admin import credentials, initialize_app
 
-firebase_creds = environ.get("OPEN_PHARMA_FIREBASE_CREDENTIALS")
+firebase_creds = literal_eval(environ.get("OPEN_PHARMA_FIREBASE_CREDENTIALS"))
 firebase_db_url = environ.get("OPEN_PHARMA_DATABASE_URL")
 
 
 class InitFirebaseConnection():
     def __init__(self):
         print("Initializing connection with Firebase...")
-        firebase_creds = credentials.Certificate("./test-28c9f-firebase-adminsdk-w2llr-6e997387b0.json")
+        firebase_creds = credentials.Certificate(firebase_creds)
         database_config = {'databaseURL': firebase_db_url}
         app = initialize_app(firebase_creds, database_config)
