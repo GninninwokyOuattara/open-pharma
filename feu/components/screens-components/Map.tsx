692721eafc0b19524d4b7e381c18800626aa6cdc
g-ouattara <idrissgouattara@gmail.com>
2022-06-25 16:29:11 +0000

    Cleaned and extended functionnalities with a button to recenter map on user position if given access

--- feu/components/screens-components/Map.tsx
+++ feu/components/screens-components/Map.tsx
@@ -1,18 +1,14 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet, Dimensions } from "react-native";
-import useLocation from "../../hooks/useLocation";
 
-import LoadingSpinner from "../utility-components/LoadingSpinner";
-import UserPositionMarker from "../utility-components/UserPositionMarker";
-import { Pharmacy, RootReducerType } from "../../types/dataTypes";
-import { LocationObject } from "expo-location";
-import usePharmaciesData from "../../hooks/usePharmaciesData";
-import { MapContext, MapContextType } from "../../contexts/MapContext";
-import { UserLocationContext } from "../../contexts/UserLocationContext";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchLocalPharmaciesData } from "../../stores/pharmaciesActions";
+import { MapContext } from "../../contexts/MapContext";
+import { UserLocationContext } from "../../contexts/UserLocationContext";
+import { RootReducerType } from "../../types/dataTypes";
+// import { fetchLocalPharmaciesData } from "../../stores/pharmaciesActions";
+
+import CustomShowsMyLocationButton from "../../components/utility-components/CustomShowsMyLocationButton";
 
 interface props {
     setIsMapLoaded: React.Dispatch<React.SetStateAction<boolean>>;
@@ -40,29 +36,17 @@ const Map: React.FC<props> = ({ setIsMapLoaded }) => {
             }}
             provider={PROVIDER_GOOGLE}
             showsUserLocation={true}
+            mapPadding={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            // mapType={"mutedStandard"}
             // showsMyLocationButton={true}
-            // mapPadding={{ top: 0, right: 0, bottom: 50, left: 0 }}
+            showsCompass={true}
+            loadingEnabled={true}
+        // mapPadding={{ top: 0, right: 0, bottom: 50, left: 0 }}
         >
-            {/* <Marker
-                key={1000}
-                coordinate={{
-                    latitude: location?.coords.latitude || 5.393620594067611,
-                    longitude: location?.coords.longitude || -4.005658558941592,
-                }}
-                title={"My marker"}
-            /> */}
+
             {pharmaciesDatas &&
                 pharmaciesDatas.map((pharmacyData, index) => {
                     const { lat, lng } = pharmacyData.coordinates;
-                    // try {
-                    // } catch (err) {
-                    //     console.log(pharmacyData);
-                    // }
-
-                    // console.log(pharmacyData.coordinates);
-                    // const [latitude, longitude] = pharmacyData.coordinates.split(
-                    //     ","
-                    // ).map((coord) => +coord);
                     return (
                         <Marker
                             key={index}
@@ -70,15 +54,19 @@ const Map: React.FC<props> = ({ setIsMapLoaded }) => {
                                 latitude: +lat,
                                 longitude: +lng,
                             }}
-                            // pinColor={
-                            //     pharmacyData.Id == selectedMarker
-                            //         ? "green"
-                            //         : "black"
-                            // }
-                            // onPress={() => console.log(pharmacyData.Position)}
+                        // pinColor={
+                        //     pharmacyData.Id == selectedMarker
+                        //         ? "green"
+                        //         : "black"
+                        // }
+                        // onPress={() => console.log(pharmacyData.Position)}
                         />
                     );
                 })}
+
+            {/* Button to point map on user position */}
+            {location && <CustomShowsMyLocationButton />}
+
         </MapView>
     );
 };
@@ -87,12 +75,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fce",
-        alignItems: "center",
-        justifyContent: "center",
+
     },
     map: {
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height,
+        flex: 1,
     },
 });
 
