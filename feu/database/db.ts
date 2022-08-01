624df4c09db454c1e09e796ec4b4cc25573b578f
g-ouattara <idrissgouattara@gmail.com>
2022-08-01 22:15:34 +0000

    create database

---
+++ feu/database/db.ts
@@ -0,0 +1,44 @@
+import * as SQLite from "expo-sqlite";
+import { Pharmacy } from "../types/dataTypes";
+
+
+const db = SQLite.openDatabase("pharmacies");
+
+export const init = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "CREATE TABLE IF NOT EXISTS pharmacies (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, _name TEXT NOT NULL, _name_safe TEXT NOT NULL, flat_name TEXT NOT NULL, geographical_position TEXT, google_maps_position_link TEXT, open INTEGER NOT NULL, open_from TEXT NOT NULL, open_until TEXT NOT NULL, phone_numbers TEXT, lat REAL, lng REAL)",
+                [],
+                () => {
+                    resolve("initialized");
+                },
+                (_, error): boolean => {
+                    reject(error);
+                    return false;
+                }
+            );
+        });
+    });
+
+    return promise;
+};
+
+
+export const insertPharmacie = (pharmacy : Pharmacy)=> {
+    const {name, _name, _name_safe, flat_name, geographical_position, google_maps_position_link, phone_numbers, supervisor, open, open_from,open_until, coordinates, phid} = pharmacy
+    const promise = new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "INSERT INTO places (name, _name, _name_safe, flat_name, geographical_position, google_maps_position_link,phone_numbers, lat, lng, open, open_from, open_until) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                [name, _name, _name_safe, flat_name, geographical_position || "", google_maps_position_link || "",JSON.stringify(phone_numbers), coordinates.lat, coordinates.lng, open ? 1 : 0, open_from || "", open_until || ""],
+                (_, res) => {
+                    resolve(res);
+                },
+                (_, error) => {
+                    reject(error);
+                    return false;
+                }
+            );
+        });
+    })}
