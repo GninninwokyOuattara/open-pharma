0f77a717f48c1e1dd869bba46efb19e24380fbeb
g-ouattara <idrissgouattara@gmail.com>
2022-08-27 23:09:15 +0000

    Added shadow

--- feu/components/screens-components/bottomsheet-components/CustomSearchBar.tsx
+++ feu/components/screens-components/bottomsheet-components/CustomSearchBar.tsx
@@ -1,12 +1,10 @@
-import { View, Text } from "react-native";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { SearchBar } from "react-native-elements";
 import { SearchBarBaseProps } from "react-native-elements/dist/searchbar/SearchBar";
 import { useDispatch, useSelector } from "react-redux";
-import { SearchBar } from "react-native-elements";
-import { useBottomSheetInternal } from "@gorhom/bottom-sheet";
 import { applyFilter } from "../../../stores/pharmaciesActions";
-import usePharmaciesData from "../../../hooks/usePharmaciesData";
 import { RootReducerType } from "../../../types/dataTypes";
+import ShadowAround from "../../utility-components/ShadowAround";
 
 const SafeSearchBar = SearchBar as unknown as React.FC<
     SearchBarBaseProps & { cancelButtonTitle?: boolean }
@@ -31,25 +29,28 @@ const CustomSearchBar = () => {
     }, [search]);
 
     return (
-        <SafeSearchBar
-            platform="ios"
-            placeholder="Rechercher une pharmacie..."
-            clearButtonMode="never"
-            onChangeText={handleChange}
-            // clearIcon={false}
-            value={search}
-            containerStyle={{
-                backgroundColor: "transparent",
-                // backgroundColor: "#F0ECD6",
-                // opacity: 0.9,
-            }}
-            inputContainerStyle={{
-                backgroundColor: "#FFF",
-                // marginLeft: 0,
-                // marginRight: 0,
-            }}
-            cancelButtonTitle={false}
-        />
+        <ShadowAround>
+
+            <SafeSearchBar
+                platform="ios"
+                placeholder="Rechercher une pharmacie..."
+                clearButtonMode="never"
+                onChangeText={handleChange}
+                // clearIcon={false}
+                value={search}
+                containerStyle={{
+                    backgroundColor: "transparent",
+                    // backgroundColor: "#F0ECD6",
+                    // opacity: 0.9,
+                }}
+                inputContainerStyle={{
+                    backgroundColor: "#FFF",
+                    // marginLeft: 0,
+                    // marginRight: 0,
+                }}
+                cancelButtonTitle={false}
+            />
+        </ShadowAround>
     );
 };
 
