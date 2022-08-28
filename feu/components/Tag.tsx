e6ff2a6e7005f29f0a4666626d392d4cfa109524
g-ouattara <idrissgouattara@gmail.com>
2022-08-28 01:02:12 +0000

    ...

--- feu/components/Tag.tsx
+++ feu/components/Tag.tsx
@@ -2,12 +2,26 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import ShadowAround from './utility-components/ShadowAround'
 
-const Tag = () => {
+interface Props {
+    title: string,
+    // color: string,
+    // action ?: () => void,
+}
+
+const Tag: React.FC<Props> = ({ title }) => {
     return (
         <ShadowAround>
 
-            <View style={{ borderWidth: 1, borderColor: "black", backgroundColor: "white", alignSelf: "flex-start", padding: 5, marginRight: 5, borderRadius: 5 }}>
-                <Text>Tag</Text>
+            <View style={{
+                borderWidth: 1,
+                borderColor: "black",
+                backgroundColor: "white",
+                alignSelf: "flex-start",
+                padding: 5,
+                marginRight: 5,
+                borderRadius: 5
+            }}>
+                <Text>{title}</Text>
             </View>
         </ShadowAround>
     )
