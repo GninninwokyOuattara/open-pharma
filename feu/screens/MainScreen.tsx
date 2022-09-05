93742fcfb8eed6999d7ccc946aa535944e1c2485
g-ouattara <idrissgouattara@gmail.com>
2022-09-05 23:42:01 +0000

    Fetch and set oph version when fetching pharmacies online

--- feu/screens/MainScreen.tsx
+++ feu/screens/MainScreen.tsx
@@ -7,9 +7,9 @@ import { useDispatch, useSelector } from "react-redux";
 import BottomBar from "../components/screens-components/BottomBar";
 import MainBottomSheet from "../components/screens-components/BottomSheet";
 import { UserLocationContext } from "../contexts/UserLocationContext";
-import { getAllPharmacies } from "../database/db";
+import { getAllPharmacies, getUpdateVersion, updateDeviceUpdateVersionTo } from "../database/db";
 import { FETCH_ALL_PHARMACIES } from "../stores/actions";
-import { calculatePharmaciesProximityToUser, fetchAllPharmacies } from "../stores/pharmaciesActions";
+import { calculatePharmaciesProximityToUser, fetchAllPharmacies, getCurrentUpdateVersion } from "../stores/pharmaciesActions";
 import { DBPharmacy, RootReducerType } from "../types/dataTypes";
 import { parsePharmacy } from "../utils/datasMorphing";
 
@@ -31,15 +31,36 @@ const MainScreen = () => {
     // On launch, retrieve data from database if exist otherwise from firebase
     useEffect(() => {
         (async () => {
+            // const update = await updateDeviceUpdateVersionTo("TEST0001")
+            // console.log("update", update);
+            // const currentVersion = await getUpdateVersion()
+            // console.log("Current version: " + currentVersion)
+
             let pharmacies: DBPharmacy[] = await getAllPharmacies()
             if (pharmacies.length > 0) {
+                console.log("Using locally stored pharmacies")
                 let pharmaciesDatas = pharmacies.map((pharmacy) => parsePharmacy(pharmacy))
                 return dispatch({
                     type: FETCH_ALL_PHARMACIES,
                     pharmaciesDatas: pharmaciesDatas
                 })
-            } else {
+
+            } else if (!pharmacies.length) {
+                // There is no pharmacies in the database yet.
+                console.log("Fetching pharmacies online")
+                // await dropPharmaciesTable()
                 await dispatch(fetchAllPharmacies())
+                const latestVersion = await getCurrentUpdateVersion()
+                await updateDeviceUpdateVersionTo(latestVersion)
+                const currentVersion = await getUpdateVersion()
+                // console.log("Latest version: " + latestVersion)
+                console.log("Current version: " + currentVersion)
+
+            } else {
+                // await dispatch(fetchAllPharmacies())
+                console.log(pharmacies)
+                console.log(!pharmacies)
+                console.log("Something went wrong")
             }
         })()
     }, [])
