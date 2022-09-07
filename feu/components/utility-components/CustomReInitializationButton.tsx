b82f5da19c3889d891a8857242da976e036aa994
g-ouattara <idrissgouattara@gmail.com>
2022-09-07 22:50:53 +0000

    Button is disabled when fetching

--- feu/components/utility-components/CustomReInitializationButton.tsx
+++ feu/components/utility-components/CustomReInitializationButton.tsx
@@ -17,38 +17,22 @@ const CustomReInitializationButton = () => {
 
     let spinValue = new Animated.Value(0);
 
-    // First set up animation
-
     const spinningAnimation = Animated.loop(Animated.timing(
         spinValue,
         {
             toValue: 1,
             duration: 3000,
-            easing: Easing.linear, // Easing is an additional import from react-native
-            useNativeDriver: true  // To make use of native driver for performance
+            easing: Easing.linear,
+            useNativeDriver: true
         }
     ))
 
-
-    // Next, interpolate beginning and end values (in this case 0 and 1)
     const spin = spinValue.interpolate({
         inputRange: [0, 1],
         outputRange: ['0deg', '360deg']
     })
 
-    const respin = () => {
-
-        spinningAnimation.start((result) => {
-            console.log("Animation done!")
-
-        })
-
-    }
-
-    // respin()
-
     useEffect(() => {
-        console.log(isFetching)
         if (isFetching) {
             spinningAnimation.start()
         } else {
@@ -59,10 +43,13 @@ const CustomReInitializationButton = () => {
 
     return (
 
-        <TouchableOpacity onPress={async () => {
-            console.log("Reinitialization")
-            await init();
-        }}>
+        <TouchableOpacity
+            onPress={async () => {
+                console.log("Reinitialization")
+                await init();
+            }}
+            disabled={!!isFetching}
+        >
             <Animated.View style={{ ...styles.iconContainer, transform: [{ rotate: spin }] }}>
 
                 <MaterialCommunityIcons
