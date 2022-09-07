e38f00b175d407287eac3e08f2dc78a259952663
g-ouattara <idrissgouattara@gmail.com>
2022-09-07 00:09:58 +0000

    use try/catch then finally to properly end fetching state whatever happen

--- feu/hooks/useInitializer.tsx
+++ feu/hooks/useInitializer.tsx
@@ -1,5 +1,6 @@
-import { useCallback } from 'react';
+import { useCallback, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { MapContext } from '../contexts/MapContext';
 import { getAllPharmacies, getUpdateVersion, updateDeviceUpdateVersionTo } from '../database/db';
 import { FETCH_ALL_PHARMACIES } from '../stores/actions';
 import { fetchAllPharmacies, getCurrentUpdateVersion } from '../stores/pharmaciesActions';
@@ -7,6 +8,8 @@ import { DBPharmacy, RootReducerType } from '../types/dataTypes';
 import { parsePharmacy } from '../utils/datasMorphing';
 
 const useInitializer = () => {
+
+    const { setIsFetching } = useContext(MapContext)
 
     const pharmaciesDatas = useSelector((state: RootReducerType) => {
         return state.pharmacies.toDisplay;
@@ -36,6 +39,7 @@ const useInitializer = () => {
         try {
             const latestVersion = await getCurrentUpdateVersion()
             await updateDeviceUpdateVersionTo(latestVersion)
+            console.log("Updated device version to " + latestVersion)
             return latestVersion
         } catch (error) {
             throw error;
@@ -44,45 +48,54 @@ const useInitializer = () => {
     }
 
     const init = useCallback(async () => {
-        let pharmacies: DBPharmacy[] = await getAllPharmacies()
-        if (pharmacies.length > 0) {
-            // const isOk = await isDeviceVersionValid()
+        setIsFetching!(true)
+        try {
+            let pharmacies: DBPharmacy[] = await getAllPharmacies()
+            if (pharmacies.length > 0) {
+                // const isOk = await isDeviceVersionValid()
 
-            console.log("Using locally stored pharmacies")
-            let pharmaciesDatas = pharmacies.map((pharmacy) => parsePharmacy(pharmacy))
-            dispatch({
-                type: FETCH_ALL_PHARMACIES,
-                pharmaciesDatas: pharmaciesDatas
-            })
-            // const isOk = await isDeviceVersionValid()
-            // console.log("isOk: " + isOk)
-            if (!await isDeviceVersionValid()) {
-                console.log("Outdated device version, fetching most recent one...")
+                console.log("Using locally stored pharmacies")
+                let pharmaciesDatas = pharmacies.map((pharmacy) => parsePharmacy(pharmacy))
+                dispatch({
+                    type: FETCH_ALL_PHARMACIES,
+                    pharmaciesDatas: pharmaciesDatas
+                })
+                // const isOk = await isDeviceVersionValid()
+                // console.log("isOk: " + isOk)
+                if (!await isDeviceVersionValid()) {
+                    console.log("Outdated device version, fetching most recent one...")
+                }
+
+
+
+                // Check if version  is ok to work with here
+
+            } else if (!pharmacies.length) {
+                // There is no pharmacies in the database yet.
+                console.log("Fetching pharmacies online")
+                // await dropPharmaciesTable()
+                await Promise.all([dispatch(fetchAllPharmacies()), updateDeviceVersion()])
+                // await dispatch(fetchAllPharmacies())
+                // await updateDeviceVersion()
+                // const latestVersion = await getCurrentUpdateVersion()
+                // await updateDeviceUpdateVersionTo(latestVersion)
+                // const currentVersion = await getUpdateVersion()
+                // console.log("Latest version: " + latestVersion)
+                // console.log("Current version: " + currentVersion)
+
+            } else {
+                // await dispatch(fetchAllPharmacies())
+                console.log(pharmacies)
+                console.log(!pharmacies)
+                console.log("Something went wrong")
             }
-
-
-
-            // Check if version  is ok to work with here
-
-        } else if (!pharmacies.length) {
-            // There is no pharmacies in the database yet.
-            console.log("Fetching pharmacies online")
-            // await dropPharmaciesTable()
-            await Promise.all([dispatch(fetchAllPharmacies()), updateDeviceVersion()])
-            // await dispatch(fetchAllPharmacies())
-            // await updateDeviceVersion()
-            // const latestVersion = await getCurrentUpdateVersion()
-            // await updateDeviceUpdateVersionTo(latestVersion)
-            // const currentVersion = await getUpdateVersion()
-            // console.log("Latest version: " + latestVersion)
-            // console.log("Current version: " + currentVersion)
-
-        } else {
-            // await dispatch(fetchAllPharmacies())
-            console.log(pharmacies)
-            console.log(!pharmacies)
-            console.log("Something went wrong")
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsFetching!(false)
         }
+
+        setIsFetching!(false)
     }, [pharmaciesDatas])
 
     return { init }
