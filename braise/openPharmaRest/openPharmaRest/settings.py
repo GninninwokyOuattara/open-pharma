4b59c0290d27c8436a42cbcb5e318fcb7bea4795
g-ouattara <idrissgouattara@gmail.com>
2022-11-16 18:37:33 +0100

    database config for postgresql

--- braise/openPharmaRest/openPharmaRest/settings.py
+++ braise/openPharmaRest/openPharmaRest/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'openPharmaRest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+# Database settings for postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'openpharmarest',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
