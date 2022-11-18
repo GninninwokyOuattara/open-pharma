f904985976873ea2a02a7e90aeeb335c16390fca
g-ouattara <idrissgouattara@gmail.com>
2022-11-18 00:01:21 +0000

    advancement

--- braise/openPharmaRest/openPharma/views.py
+++ braise/openPharmaRest/openPharma/views.py
@@ -3,6 +3,7 @@ from openPharma.models import OpenPharmacy, Pharmacy
 from openPharma.serializers import (PharmaciesAdminSerializer,
                                     PharmaciesSerializer)
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
@@ -14,3 +15,27 @@ class PharmaciesViewset(viewsets.ReadOnlyModelViewSet):
 class PharmaciesAdminViewset(viewsets.ModelViewSet):
     queryset = Pharmacy.objects.all()
     serializer_class = PharmaciesAdminSerializer
+
+    def get_queryset(self):
+        # Get query all params
+
+        return super().get_queryset()
+
+    # Create a desactivate action
+    @action(detail=True, methods=['post'])
+    def desactivate(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.active = False
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    # Create an activate action
+    @action(detail=True, methods=['post'])
+    def activate(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.active = True
+        instance.pending_review = False
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
