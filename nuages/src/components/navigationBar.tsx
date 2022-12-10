863fac12fc4a8507a6166b21d6dea7ee7c239f8d
g-ouattara <idrissgouattara@gmail.com>
2022-12-10 21:54:03 +0000

    save

--- nuages/src/components/navigationBar.tsx
+++ nuages/src/components/navigationBar.tsx
@@ -1,7 +1,10 @@
 // Navigation bar component
 
-import { Box, Icon, Link, VStack } from "@chakra-ui/react";
+import { Box, Icon, List, ListItem, VStack } from "@chakra-ui/react";
 import { MdSettings } from 'react-icons/md';
+
+// import Link from router
+import { Link } from 'react-router-dom';
 
 
 
@@ -10,27 +13,54 @@ const NavigationBar = () => {
         // A Box of black color with padding of 10 that take all the available height
         <Box
             backgroundColor="black"
-            padding={10}
+            // padding={10}
             height="100%"
-            width={15}
+            width={"100px"}
+            paddingY={5}
             borderRadius="md"
             overflow={"hidden"}
         >
 
-            <VStack spacing={10}>
-                <Link href="/" display={"block"} color="gray.400" _hover={{ color: "white", background: "gray" }}>
+            <VStack spacing={10} w={"full"}>
 
-                    <Icon as={MdSettings} />
 
-                </Link>
-                <Link href="/" display={"block"} color="gray.400" _hover={{ color: "white" }}>
-                    <Icon as={MdSettings} />
+                <List width={"full"}>
+                    <ListItem w={"full"} h={"40px"} justifyContent="center" alignItems="center" _hover={{
+                        backgroundColor: "gray.700",
 
-                </Link>
-                <Link href="/" display={"block"} color="gray.400" _hover={{ color: "white" }}>
-                    <Icon as={MdSettings} />
+                    }}
+                        _groupHover={{
+                            bg: "gray.500",
+                            color: "white"
+                        }}>
+                        <Link to={"/"} style={{ display: "block" }} >
+                            <Icon as={MdSettings} w="full" _hover={{
+                                color: "white",
+                            }} />
+                        </Link>
+                    </ListItem>
+                    <ListItem
+                        _hover={{
+                            bg: "gray.500",
+                            color: "orange",
+                        }}
+                    >
+                        <Icon
+                            as={MdSettings} w="full"
+                            _hover={{
+                                color: "orange",
+                            }}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <Link to={"/"}>
+                            <Icon as={MdSettings} />
+                        </Link>
+                    </ListItem>
+                </List>
 
-                </Link>
+
+
             </VStack>
 
         </Box >
