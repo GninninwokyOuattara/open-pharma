738cfd022b5f490d67f593c44ac23d5a2d727ca8
g-ouattara <idrissgouattara@gmail.com>
2022-12-11 14:48:21 +0000

    define route with icons

--- nuages/src/routes.ts
+++ nuages/src/routes.ts
@@ -1,7 +1,12 @@
+import { AiOutlineUnorderedList } from "react-icons/ai";
+import { RxDashboard } from "react-icons/rx";
+
+import { FaTasks } from "react-icons/fa";
+
 const routes = [
-  { path: "/", name: "home" },
-  { path: "/pharmacies", name: "about" },
-  { path: "/pending-reviews", name: "contact" },
+  { path: "/", name: "home", icon: RxDashboard },
+  { path: "/pharmacies", name: "about", icon: AiOutlineUnorderedList },
+  { path: "/pending-reviews", name: "contact", icon: FaTasks },
 ];
 
 export default routes;
