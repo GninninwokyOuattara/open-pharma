d1a3e65793a0a265b1cdabf94686a093d6065a31
g-ouattara <idrissgouattara@gmail.com>
2022-12-19 00:05:43 +0000

    advancement are beautiful so far

--- nuages/src/components/pages/pendingReviewsPage.tsx
+++ nuages/src/components/pages/pendingReviewsPage.tsx
@@ -2,8 +2,9 @@
 
 
 import { CheckIcon, ChevronDownIcon, CloseIcon } from '@chakra-ui/icons';
-import { Button, HStack, IconButton, Input, Menu, MenuButton, MenuItem, MenuList, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { Box, Button, HStack, IconButton, Input, Menu, MenuButton, MenuItem, MenuList, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from '@chakra-ui/react';
+import { useCallback, useEffect } from 'react';
+import { Pharmacy } from '../../types';
 import { getTimeElapsed } from '../../utils/dry';
 
 
@@ -22,6 +23,65 @@ const PendingReviews = () => {
         fetchPharmaciesPendingReview()
 
     }, [])
+
+    const renderer = useCallback(() => {
+        if (pharmaciesPendingReview) {
+
+            const pharmacies = pharmaciesPendingReview.filter((pharmacy) => pharmacy.name.toLowerCase().includes(search.toLowerCase()))
+
+            // if (pharmacies ) {
+            //     pharmacies.map((pharmacy) => {
+            //         return PendingRow({ pharmacy, activatePharmacy, deactivatePharmacy })
+            //     })
+            // }
+
+            if (pharmacies.length) {
+
+                return (
+
+                    <TableContainer style={{ height: "100%", width: "100%" }} overflowY={"scroll"} overflowX={"scroll"} marginTop={"30px"}>
+                        <Table variant='simple' >
+
+                            <Thead >
+                                <Tr >
+
+                                    <Th style={{ position: "sticky", top: 0, overflow: "hidden", backgroundColor: "white" }}>Name</Th>
+                                    <Th style={{ position: "sticky", top: 0, overflow: "hidden", backgroundColor: "white" }}>Date added</Th>
+                                    <Th style={{ position: "sticky", top: 0, overflow: "hidden", backgroundColor: "white", zIndex: 100 }}></Th>
+
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+
+
+                                {pharmacies.map((pharmacy, idx) => {
+                                    return PendingRow({ pharmacy, activatePharmacy, deactivatePharmacy })
+                                })}
+
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
+
+                )
+
+            } else {
+                return (
+                    // <Box w={"full"} alignItems={"center"} justifyItems={"center"}>
+
+                    <>
+                        <Text fontSize='5xl' color={"gray.500"}>No matching pharmacies found.</Text>
+                        <Text fontSize={"4xl"} color={"orange.400"}>Try adjusting your filter.</Text>
+                    </>
+                    // </Box>
+                )
+
+
+            }
+        } else {
+            return <Box></Box>
+        }
+    }, [search])
+
 
 
     return (
@@ -48,8 +108,12 @@ const PendingReviews = () => {
                     </Menu>
                 </HStack>
 
+                {
+                    renderer()
+                }
 
-                <TableContainer style={{ height: "100%", width: "100%" }} overflowY={"scroll"} overflowX={"scroll"} marginTop={"30px"}>
+
+                {/* <TableContainer style={{ height: "100%", width: "100%" }} overflowY={"scroll"} overflowX={"scroll"} marginTop={"30px"}>
                     <Table variant='simple' >
 
                         <Thead >
@@ -62,31 +126,11 @@ const PendingReviews = () => {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {pharmaciesPendingReview && pharmaciesPendingReview.filter((pharmacy) => pharmacy.name.toLowerCase().includes(search.toLowerCase())).map((pharmacy, idx) => {
-                                console.log("pharmacy: ", pharmacy)
-                                const timeElapsedFromCreationCreation = getTimeElapsed(pharmacy.date_created)
-                                return (
-                                    <Tr key={pharmacy.id} className={styles.tableRow}>
-                                        <Td width={"100%"}>{pharmacy.name}</Td>
-
-                                        <Td>{timeElapsedFromCreationCreation}</Td>
-
-                                        <Td className={styles.tableDataHidden}>
-
-                                            <HStack gap={2} alignSelf={"flex-end"}>
-                                                <IconButton aria-label='Activate pharmacy' icon={<CheckIcon />} onClick={() => activatePharmacy(pharmacy)} />
-                                                <IconButton aria-label='Deactivate pharmacy' icon={<CloseIcon />} onClick={() => deactivatePharmacy(pharmacy)} />
-                                            </HStack>
-                                        </Td>
-
-                                    </Tr>
-                                )
-                            })
-                            }
+                            {renderer()}
 
                         </Tbody>
                     </Table>
-                </TableContainer>
+                </TableContainer> */}
 
 
 
@@ -99,3 +143,45 @@ const PendingReviews = () => {
 
 
 export default PendingReviews;
+
+
+
+
+
+interface PendingRowProps {
+    pharmacy: Pharmacy,
+    activatePharmacy: (pharmacy: Pharmacy) => void,
+    deactivatePharmacy: (pharmacy: Pharmacy) => void
+}
+
+export const PendingRow = ({ pharmacy, activatePharmacy, deactivatePharmacy }: PendingRowProps) => {
+
+    const timeElapsedFromCreationCreation = getTimeElapsed(pharmacy.date_created)
+
+    return (
+
+
+
+
+        <Tr key={pharmacy.id} className={styles.tableRow}>
+            <Td width={"100%"}>{pharmacy.name}</Td>
+
+            <Td>{timeElapsedFromCreationCreation}</Td>
+
+            <Td className={styles.tableDataHidden}>
+
+                <HStack gap={2} alignSelf={"flex-end"}>
+                    <IconButton aria-label='Activate pharmacy' icon={<CheckIcon />} onClick={() => activatePharmacy(pharmacy)} />
+                    <IconButton aria-label='Deactivate pharmacy' icon={<CloseIcon />} onClick={() => deactivatePharmacy(pharmacy)} />
+                </HStack>
+            </Td>
+
+        </Tr>
+
+    )
+
+}
+
+
+
+
