99739985974c9f7e5e6e4e6b4ab071d00b91c679
g-ouattara <idrissgouattara@gmail.com>
2022-12-24 19:50:20 +0000

    add ordering

--- braise/openPharmaRest/openPharma/models.py
+++ braise/openPharmaRest/openPharma/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Pharmacy(models.Model):
     class Meta:
         db_table = 'pharmacy'
+        ordering = ['name', 'date_created', 'active']
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
