7eba18e5baed5dfdfff9bdf9d33ea978cd37fa6d
g-ouattara <idrissgouattara@gmail.com>
2022-12-25 16:25:34 +0000

    looking pretty

--- nuages/src/pages/pharmaciesPage.tsx
+++ nuages/src/pages/pharmaciesPage.tsx
@@ -1,5 +1,9 @@
-import { Box, Flex, Text, VStack } from "@chakra-ui/react";
-import { AiOutlineFieldTime } from "react-icons/ai";
+import { Box, Flex, Input, Skeleton, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr, VStack } from "@chakra-ui/react";
+import { AiOutlineEye } from "react-icons/ai";
+import { MdOutlineHouseSiding } from "react-icons/md";
+import { RiEyeOffLine } from "react-icons/ri";
+import usePharmacies from "../hooks/usePharmacies";
+import { PharmaciesDataSummary, PharmacyFullState } from "../types";
 
 
 
@@ -7,11 +11,8 @@ import { AiOutlineFieldTime } from "react-icons/ai";
 
 const PharmaciesPage = () => {
 
-    // get the environment variable REACT_APP_DJANGO_API_URL
-    const REACT_APP_DJANGO_API_URL = process.env.REACT_APP_DJANGO_API_URL;
-    console.log("REACT_APP_DJANGO_API_URL: ", REACT_APP_DJANGO_API_URL);
-
-
+    const { isLoading, summary, pharmacies, error } = usePharmacies()
+    console.log(pharmacies)
 
 
 
@@ -19,8 +20,13 @@ const PharmaciesPage = () => {
 
     return (
         <>
-            <DataRecapCard data={100} icon={<AiOutlineFieldTime size={"40px"} color={"#2B7DBF"} />} iconBg={"#E8F3FF"} header={"Pending Reviews"} width={"300px"} height={"100px"} />
+            <VStack height={"100%"} width={"100%"}>
 
+                <RecapContainer summary={summary} isLoading={isLoading} />
+                <Box height={10}></Box>
+                <PharmaciesTableContainer pharmacies={pharmacies} isLoading={isLoading} />
+
+            </VStack>
 
         </>
     );
@@ -30,21 +36,120 @@ export default PharmaciesPage;
 
 
 
+
+
+const PharmaciesTableContainer = ({ pharmacies, isLoading }: { pharmacies: PharmacyFullState[], isLoading: boolean }) => {
+
+
+    return (
+        <TableContainer shadow={"md"} borderRadius={"md"} width="full" height="full" >
+            <VStack width={"full"} height="full">
+
+                <Box width={"full"} backgroundColor={"#F8FBFC"} padding={1} top={0} position={"sticky"} zIndex={100} height={"50px"}>
+                    <VStack>
+                        <Input placeholder='Search by name'
+                            width={200}
+                            alignSelf={"flex-start"}
+                            shadow={"md"}
+                        />
+
+
+                    </VStack>
+
+                </Box>
+                <Box overflowY={"scroll"} height={"100%"} width="full">
+
+                    <Table variant='simple'>
+
+                        <Thead position={"sticky"} top={0} zIndex={100} backgroundColor={"#F8FBFC"}>
+                            <Tr>
+                                <Th>Name</Th>
+                                <Th>State</Th>
+                                <Th>Opening</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody >
+
+                            {!isLoading && pharmacies.length ? pharmacies.map((pharmacy) => {
+                                return <Tr>
+                                    <Td>{pharmacy.name}</Td>
+                                    <Td>
+                                        <StateTag />
+                                        <StateTag />
+                                    </Td>
+                                    <Td >{pharmacy.open_date_range?.date_range_string || "-"}</Td>
+                                </Tr>
+                            }) : 0}
+
+                            <Tr>
+                                <Td>Pharmacy Sainte Cecile</Td>
+                                <Td>
+                                    <StateTag />
+                                    <StateTag />
+                                </Td>
+                                <Td >Decembre - Janvier</Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+            </VStack>
+
+        </TableContainer>
+    )
+}
+
+
+
+
+
+const StateTag = ({ state, backgroundColor, color }: { state?: string, backgroundColor?: string, color?: string }) => {
+    state = state || "Unknown"
+    backgroundColor = backgroundColor || "#F2FBFF"
+    color = color || "#ACBCD3"
+    return <Box display={"inline-block"} padding={2} borderRadius={"lg"} backgroundColor={backgroundColor} color={color}>{state}</Box>
+
+}
+
+
+
+const RecapContainer = ({ summary, isLoading }: { summary?: PharmaciesDataSummary, isLoading: boolean }) => {
+
+    return <Flex direction={"row"} gap={2} marginTop={5}>
+        <DataRecapCard data={summary?.inactive_Pharmacies_count || 0} icon={<RiEyeOffLine size={"40px"} color={"#2B7DBF"} />} iconBg={"#E8F3FF"} header={"Inactive Pharmacies"} isLoading={isLoading} />
+        <DataRecapCard data={summary?.active_pharmacies_count || 0} icon={<AiOutlineEye size={"40px"} color={"#2B7DBF"} />} iconBg={"#E8F3FF"} header={"Active Pharmacies"} isLoading={isLoading} />
+        <DataRecapCard data={summary?.open_pharmacies_count || 0} icon={<MdOutlineHouseSiding size={"40px"} color={"#18978C"} />} iconBg={"#E7FCFE"} header={"Open Pharmacies"} isLoading={isLoading} />
+
+
+    </Flex>
+
+}
+
+
+
+
 interface dataRecapCardProps {
     data: Number | string,
     header: string,
     icon: JSX.Element,
     iconBg?: string,
     width?: string
-    height?: string
+    height?: string,
+    isLoading: boolean
 }
 
-const DataRecapCard = ({ data, icon, iconBg, header, width, height }: dataRecapCardProps) => {
+
+const DataRecapCard = ({ data, icon, iconBg, header, width, height, isLoading }: dataRecapCardProps) => {
 
     width = width || "300px"
     height = height || "100px"
     iconBg = iconBg || "#E8F3FF"
 
+
+    if (isLoading) {
+        return <Skeleton width={width} height={height} borderRadius={"md"}>
+
+        </Skeleton>
+    }
 
     return (
         <Box width={width || "300px"} height={height || "100px"} bg={"white"} shadow={"md"} borderRadius={"md"} padding={5} css={{
