2241640dc504b55ca4a194fc94cfb70edb61b0db
g-ouattara <idrissgouattara@gmail.com>
2023-02-12 16:51:32 +0000

    a nice toast context

--- nuages/src/contexts/toast.tsx
+++ nuages/src/contexts/toast.tsx
@@ -3,19 +3,47 @@
 // import toast from chakra-ui
 
 import { useToast } from "@chakra-ui/react";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useCallback } from "react";
 
 interface props {
     children: ReactNode;
 }
 
-export const ToastContext = createContext(null);
+export interface ToastContextInterface {
+    successToast: (title: string, message: string) => void;
+    errorToast: (title: string, message: string) => void;
+
+}
+
+export const ToastContext = createContext<ToastContextInterface | null>(null);
 
 export const ToastProvider: React.FC<props> = ({ children }) => {
     const Toast: any = useToast();
 
+    const successToast = useCallback((title: string, message: string) => {
+        Toast({
+            title: title || "Success",
+            description: message,
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+            position: 'top-right'
+        });
+    }, [Toast])
+
+    const errorToast = useCallback((title: string, message: string) => {
+        Toast({
+            title: title || "Error",
+            description: message,
+            status: "error",
+            duration: 2000,
+            isClosable: true,
+            position: 'top-right'
+        });
+    }, [Toast])
+
     return (
-        <ToastContext.Provider value={Toast}>
+        <ToastContext.Provider value={{ successToast, errorToast }}>
             {children}
         </ToastContext.Provider>
     );
