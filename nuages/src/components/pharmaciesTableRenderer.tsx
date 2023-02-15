4b012515b2eebcd57412fe50b5ffeea5c1eff99b
g-ouattara <idrissgouattara@gmail.com>
2023-02-15 22:20:58 +0000

    table renderer

---
+++ nuages/src/components/pharmaciesTableRenderer.tsx
@@ -0,0 +1,56 @@
+import { Box, Table, TableContainer, Th, Thead, Tr } from "@chakra-ui/react"
+import PharmaciesTableBody from "./pharmaciesTableBody"
+
+
+const PharmaciesTableRenderer = () => {
+
+    return (
+
+
+        <TableContainer shadow={"outline"} borderRadius={"md"} width="full" height="full" backgroundColor={"white"}>
+            <Box height={"full"} width={"full"} overflow={"scroll"} >
+
+                <Table
+                    variant='simple'
+
+                >
+
+                    <PharmaciesTableHeaders />
+                    <PharmaciesTableBody />
+
+
+
+                </Table>
+            </Box>
+
+        </TableContainer>
+
+    )
+}
+
+export default PharmaciesTableRenderer
+
+
+
+const PharmaciesTableHeaders = () => {
+
+    return (
+        <Thead
+            backgroundColor={"gray.100"}
+            fontWeight={"bold"}
+            position={"sticky"} top={0} zIndex={1}
+
+        >
+            <Tr>
+                <Th>Name</Th>
+                <Th>Status</Th>
+                <Th>Open from</Th>
+                <Th>Open until</Th>
+                <Th padding={0} ></Th> {/* Activate/Deactivate */}
+                <Th padding={1}></Th> {/* Edit */}
+                <Th padding={1}></Th> {/* Point on map */}
+            </Tr>
+        </Thead>
+    )
+}
+
