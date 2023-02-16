aac9812d87bf01133bb40081e3d3c1e1cfdcb85e
g-ouattara <idrissgouattara@gmail.com>
2023-02-16 22:47:23 +0000

    Added more colors

--- nuages/src/colorPalette.ts
+++ nuages/src/colorPalette.ts
@@ -8,4 +8,12 @@ export const palette = {
     light: "#ffc947",
     dark: "#c66900",
   },
+  colorHuntTheme: {
+    orange: "#FAAB78",
+    lightOrange: "#FFDCA9",
+    strongOrange: "#FFB100",
+    tameOrange: "#FBC252",
+    lightGreen: "#F0ECCF",
+    green: "#A3BB98",
+  },
 };
