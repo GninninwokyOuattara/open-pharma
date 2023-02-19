a3534987adefc77497ef94d8cc448fc7f0f7b980
g-ouattara <idrissgouattara@gmail.com>
2023-02-19 23:50:38 +0000

    changes icon

--- nuages/src/routes.ts
+++ nuages/src/routes.ts
@@ -1,14 +1,14 @@
-import { AiOutlineUnorderedList } from "react-icons/ai";
 import { RxDashboard } from "react-icons/rx";
 
 import { FaTasks } from "react-icons/fa";
+import { MdOutlineLocalPharmacy } from "react-icons/md";
 
 const routes = [
   { path: "/dashboard", name: "Dashboard", icon: RxDashboard },
   {
     path: "/pharmacies",
     name: "Pharmacies List",
-    icon: AiOutlineUnorderedList,
+    icon: MdOutlineLocalPharmacy,
   },
   { path: "/pending-reviews", name: "Awaiting Reviews", icon: FaTasks },
 ];
