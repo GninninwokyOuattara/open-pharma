10de75be829662f9e5ea71a71635f23dbc4f2187
g-ouattara <idrissgouattara@gmail.com>
2023-03-04 21:00:51 +0000

    this fix the coordinates to updating

--- nuages/src/components/editPharmacyModal.tsx
+++ nuages/src/components/editPharmacyModal.tsx
@@ -3,6 +3,7 @@ import { useCallback, useContext, useEffect, useState } from "react";
 import { PharmaciesContext, PharmaciesContextInterface } from "../contexts/pharmaciesContext";
 import { ToastContext, ToastContextInterface } from "../contexts/toast";
 import { PharmacyFullStateEdit } from "../types";
+import { isValidCoordinateValue } from "../utils/dry";
 
 // import Lorem component
 
@@ -47,28 +48,33 @@ const EditPharmacyModal: React.FC<EditPharmacyModalProps> = ({ isOpen, onClose }
 
         const key: "latitude" | "longitude" | string = Object.keys(change)[0]
         let value: string = Object.values(change)[0]
+        setPharmacyForm((prevFormState) => {
+            return { ...prevFormState, [key]: value }
+        })
+    }
 
-        console.log("Key", key)
-        if (["latitude", "longitude"].includes(key)) {
-            let coordinateKey: "latitude" | "longitude" = key as "latitude" | "longitude"
-            console.log("Value", value)
-            value = value.replace(",", ".")
-            setPharmacyForm((prevFormState) => {
-
-                return { ...prevFormState, coordinates: { ...prevFormState.coordinates, [key]: value } }
-            }
-            )
-
-        } else {
-            setPharmacyForm((prevFormState) => {
-                return { ...prevFormState, [key]: value }
-            })
+    const isValidEdit = () => {
+        const { latitude, longitude } = pharmacyForm
+        const { name } = pharmacyForm
+        if (name === "") {
+            errorToast("Error", "Name cannot be empty")
+            return false
         }
+        if (isValidCoordinateValue(latitude) && isValidCoordinateValue(longitude)) {
+            return true
+        } else {
+            errorToast("", "Coordinates must be valid float numbers")
+            return false
+        }
+
     }
 
     const updatePharmacy = useCallback(async () => {
         // Put request to update pharmacy
+        if (!isValidEdit()) {
+            return
 
+        }
         const options = {
             method: "PUT",
             headers: {
@@ -129,11 +135,11 @@ const EditPharmacyModal: React.FC<EditPharmacyModalProps> = ({ isOpen, onClose }
                                     <Box display={"flex"} gap={1}>
                                         <Input
                                             placeholder={`Latitude`}
-                                            value={pharmacyForm.coordinates!.latitude}
+                                            value={pharmacyForm.latitude}
                                             onChange={(e) => handleFormChange(
                                                 { latitude: e.target.value }
                                             )} />
-                                        <Input placeholder={`Longitude`} value={pharmacyForm.coordinates!.longitude}
+                                        <Input placeholder={`Longitude`} value={pharmacyForm.longitude}
                                             onChange={(e) => handleFormChange(
                                                 { longitude: e.target.value }
                                             )} />
