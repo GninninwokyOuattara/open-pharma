d22d02dfa6b6293869712c96c41c549967806657
g-ouattara <idrissgouattara@gmail.com>
2023-03-28 23:47:09 +0000

    Skeleton loader made right !

--- feu/components/utility-components/SkeletonContentLoader.tsx
+++ feu/components/utility-components/SkeletonContentLoader.tsx
@@ -3,51 +3,41 @@ import ContentLoader, { Rect } from 'react-content-loader/native';
 
 
 import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 
 const SkeletonContentLoader = () => {
+
 
   const deviceWidth = Dimensions.get('window').width;
   const deviceHeight = Dimensions.get('window').height;
 
+
+
   const MyLoader = () => (
-    <ContentLoader
-      viewBox="0 0 100 100%"
-      speed={2}
-      // backgroundColor='#F5EBEB'
-      foregroundColor='#F4B183'
+    <ContentLoader viewBox={`0 0 ${deviceWidth} ${deviceHeight}`}
 
     >
-      {/* Only SVG shapes */}
-      <Rect x="0" y="0" rx="5" ry="5" width="100%" height="50" />
-      <Rect x="0" y="60" rx="5" ry="5" width="100%" height="50" />
-      <Rect x="0" y="120" rx="5" ry="5" width="100%" height="50" />
-      <Rect x="0" y="180" rx="5" ry="5" width="100%" height="50" />
-      <Rect x="0" y="240" rx="5" ry="5" width="100%" height="50" />
+      <Rect x="10" y="0" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="60" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="120" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="180" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="240" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="300" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="360" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="420" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="480" rx="4" ry="4" width={deviceWidth - 20} height="50" />
+      <Rect x="10" y="540" rx="4" ry="4" width={deviceWidth - 20} height="50" />
     </ContentLoader>
   )
   return (
-    <View style={styles.container}>
+    <View style={{ height: deviceHeight, width: deviceWidth }}
+    >
       <MyLoader />
     </View >
   )
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    height: 300,
-    paddingHorizontal: 10,
-    // paddingTop: 10,
-    borderRadius: 10,
-    // alignItems: "flex-start"
-    // width: "100%",
-    // overflow: "hidden",
-    // borderWidth: 1,
-    // borderColor: "red",
-
-  }
-})
 
 
 
