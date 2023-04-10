23832ca7b57ff2bcea26b85af1975fc75c0d20d1
g-ouattara <idrissgouattara@gmail.com>
2023-04-10 00:39:53 +0000

    legit

--- feu/components/CustomMarker.tsx
+++ feu/components/CustomMarker.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Image } from 'react-native'
 import { Marker } from 'react-native-maps'
 import Pulse from './utility-components/Pulse'
+import SpinningDashedCircle from './utility-components/SpinningDashedCircle'
 
 interface Props {
     id: any,
@@ -29,8 +30,9 @@ let CustomMarker: React.FC<Props> = ({ id, coordinate, open, title, selected }) 
 
             >
 
-                {/* <Image source={require("../assets/markerGreen.png")} style={{ height: 45, width: 20 }} /> */}
-                <Pulse color='blue' dotSize={15} />
+                {/* <Pulse color='blue' dotSize={15} /> */}
+                <SpinningDashedCircle />
+
 
             </Marker>
         )
