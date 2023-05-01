8836948b83881db26c1807a01791663fc20d355b
g-ouattara <idrissgouattara@gmail.com>
2023-05-01 13:27:12 +0000

    Custom Toasts

--- nuages/src/contexts/toast.tsx
+++ nuages/src/contexts/toast.tsx
@@ -2,8 +2,11 @@
 
 // import toast from chakra-ui
 
-import { useToast } from "@chakra-ui/react";
-import { createContext, ReactNode, useCallback } from "react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
+import { Box, HStack, Icon, Text, useToast } from "@chakra-ui/react";
+import { ReactNode, createContext, useCallback } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { palette } from "../colorPalette";
 
 interface props {
     children: ReactNode;
@@ -22,23 +25,48 @@ export const ToastProvider: React.FC<props> = ({ children }) => {
 
     const successToast = useCallback((title: string, message: string) => {
         Toast({
-            title: title || "Success",
+            // title: title || "Success",
             description: message,
             status: "success",
             duration: 2000,
             isClosable: true,
-            position: 'top-right'
+            position: 'top',
+            render: () => (
+                <Box borderRadius={"md"} color='white' p={3} bg={palette.custom.niceOrange}>
+                    <HStack gap={2}>
+
+                        <CheckCircleIcon color={"green.400"} />
+                        <Text fontWeight={"bold"}>
+
+                            {message}
+                        </Text>
+                    </HStack>
+                </Box>
+            ),
+
         });
     }, [Toast])
 
     const errorToast = useCallback((title: string, message: string) => {
         Toast({
-            title: title || "Error",
+            // title: title || "Error",
             description: message,
             status: "error",
             duration: 2000,
             isClosable: true,
-            position: 'top-right'
+            position: 'top',
+            render: () => (
+                <Box borderRadius={"md"} color='white' p={3} bg={palette.custom.niceOrange}>
+                    <HStack gap={2}>
+
+                        <Icon as={AiFillCloseCircle} color={"red.400"} />
+                        <Text fontWeight={"bold"}>
+
+                            {message}
+                        </Text>
+                    </HStack>
+                </Box>
+            ),
         });
     }, [Toast])
 
