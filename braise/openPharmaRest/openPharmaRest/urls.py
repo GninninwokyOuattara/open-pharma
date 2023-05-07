1b6c088bd64b7a9e81119b27deb092ebcdf8dde8
g-ouattara <idrissgouattara@gmail.com>
2023-05-07 19:38:45 +0000

    add urls to support latest dashboard views

--- braise/openPharmaRest/openPharmaRest/urls.py
+++ braise/openPharmaRest/openPharmaRest/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+
 # import SearchApiView
 from googleMapsScrapper.views import SearchApiView
 from openPharma.views import (OpenPharmaciesAdminViewset,
@@ -23,12 +26,9 @@ from openPharma.views import (OpenPharmaciesAdminViewset,
                               PharmaciesCurrentStateViewset,
                               PharmaciesPendingReviewAdminViewset,
                               PharmaciesStateAndCountViewset,
-                              PharmaciesViewset)
-from openTracker.views import CurrentlyOpenPharmaciesView
-from rest_framework import routers
-from rest_framework_swagger.views import get_swagger_view
-
+                              PharmaciesStatisticsViewset, PharmaciesViewset)
 from openPharmaRest.views import TestView
+from openTracker.views import CurrentlyOpenPharmaciesView
 
 user_router = routers.SimpleRouter()
 user_router.register(r'pharmacies', PharmaciesViewset, basename='pharmacies')
@@ -51,6 +51,14 @@ admin_router.register(r"get-pharmacies-states-count",
 admin_router.register(r"get-pharmacies-state-and-count",
                       PharmaciesStateAndCountViewset, basename="get-pharmacies-state-and-count")
 
+# admin_router.register(
+#     r"dashboard/get-pharmacies-over-weeks", PharmaciesStatisticsViewset.as_view({
+#         "get": "get_pharmacies_over_weeks"
+#     }),
+
+#     basename="dashboard"
+# )
+
 schema_view = get_swagger_view(title='OpenPharma API')
 
 urlpatterns = [
@@ -59,6 +67,16 @@ urlpatterns = [
     path("admin-api/", include(admin_router.urls)),
     path("admin-api/get-currently-open-pharmacies/",
          CurrentlyOpenPharmaciesView.as_view(), name="tracker"),
+    path("admin-api/dashboard/get-pharmacies-over-weeks/", PharmaciesStatisticsViewset.as_view({
+        "get": "get_pharmacies_over_weeks"
+    })),
+    path("admin-api/dashboard/get-pharmacies-states/", PharmaciesStatisticsViewset.as_view({
+        "get": "get_pharmacies_states"
+    })),
+    path("admin-api/dashboard/get-pharmacies-reviews-states/", PharmaciesStatisticsViewset.as_view({
+        "get": "get_reviews_states"
+    })),
+
     path("search/",
          SearchApiView.as_view(), name="search"),
 
