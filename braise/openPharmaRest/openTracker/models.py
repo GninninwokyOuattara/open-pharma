14973059cf90f05f24b8c2e366dbc5b0efa2d1f9
g-ouattara <idrissgouattara@gmail.com>
2023-05-08 22:00:32 +0000

    add new  column

--- braise/openPharmaRest/openTracker/models.py
+++ braise/openPharmaRest/openTracker/models.py
@@ -16,5 +16,19 @@ class TrackerHistory(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     collected_data = models.JSONField()
+    inserted_pharmacies = models.IntegerField(
+        blank=True, null=True, default=0
+    )
+    updated_pharmacies = models.IntegerField(
+        blank=True, null=True, default=0
+
+    )
+    skipped_pharmacies = models.IntegerField(
+        blank=True, null=True, default=0
+
+    ),
+    already_open_pharmacies = models.IntegerField(
+        blank=True, null=True, default=0
+    )
     # field 'mode' which is either 'manual' or 'automatic'
     mode = models.CharField(max_length=10)
