60c0126f2811d79d1d6163c0b816d5855fc19e55
g-ouattara <idrissgouattara@gmail.com>
2023-05-08 23:25:19 +0000

    support for summary recap

--- nuages/src/contexts/dashboardContext.tsx
+++ nuages/src/contexts/dashboardContext.tsx
@@ -16,6 +16,20 @@ interface ChartType {
     "fill": string
 }
 
+interface UpdateSummary {
+    id: string,
+    start_time: string,
+    end_time: string,
+    duration: string,
+    date_created: Date,
+    date_updated: Date,
+    inserted_pharmacies: number,
+    updated_pharmacies: number,
+    skipped_pharmacies: number,
+    already_open_pharmacies: number,
+    mode: "manual" | "automatic",
+}
+
 export interface DashboardContextInterface {
     isLoading: Boolean,
     setIsLoading: React.Dispatch<React.SetStateAction<Boolean>>,
@@ -27,6 +41,8 @@ export interface DashboardContextInterface {
     setStateChartDatas: React.Dispatch<React.SetStateAction<ChartType[]>>,
     activities: ActivitiesType[],
     setActivities: React.Dispatch<React.SetStateAction<ActivitiesType[]>>,
+    updateSummary: UpdateSummary,
+    setUpdateSummary: React.Dispatch<React.SetStateAction<UpdateSummary>>,
 
 }
 
@@ -44,6 +60,19 @@ export const DashboardContextProvider = ({ children }: any) => {
     const [reviewChartDatas, setReviewChartDatas] = useState<ChartType[]>([])
     const [stateChartDatas, setStateChartDatas] = useState<ChartType[]>([])
     const [activities, setActivities] = useState<ActivitiesType[]>([])
+    const [updateSummary, setUpdateSummary] = useState<UpdateSummary>({
+        id: "",
+        start_time: "",
+        end_time: "",
+        duration: "",
+        date_created: new Date(),
+        date_updated: new Date(),
+        inserted_pharmacies: 0,
+        updated_pharmacies: 0,
+        skipped_pharmacies: 0,
+        already_open_pharmacies: 0,
+        mode: "manual",
+    })
 
 
     const getStatistics = async () => {
@@ -51,27 +80,47 @@ export const DashboardContextProvider = ({ children }: any) => {
         try {
 
 
-            const [lineChartResponse, reviewChartResponse, stateChartResponse, ActivitiesResponse] = await Promise.all([
+            const [lineChartResponse, reviewChartResponse, stateChartResponse, ActivitiesResponse, updateSummary] = await Promise.all([
                 fetch(`${backendUrl}/admin-api/dashboard/get-pharmacies-over-weeks/`),
                 fetch(`${backendUrl}/admin-api/dashboard/get-pharmacies-reviews-states/`),
                 fetch(`${backendUrl}/admin-api/dashboard/get-pharmacies-states/`),
                 fetch(`${backendUrl}/admin-api/dashboard/get-recent-activity/`),
+                fetch(`${backendUrl}/admin-api/dashboard/get-latest-tracker-results/`),
+
             ])
 
             const lineChartDatas = await lineChartResponse.json()
             const reviewChartDatas = await reviewChartResponse.json()
             const stateChartDatas = await stateChartResponse.json()
             const ActivitiesDatas = await ActivitiesResponse.json()
+            const updateSummaryDatas = await updateSummary.json()
+
 
             setLineChartDatas(lineChartDatas)
             setReviewChartDatas(reviewChartDatas)
             setStateChartDatas(stateChartDatas)
             setActivities(ActivitiesDatas)
+            console.log("", updateSummary)
+
+            setUpdateSummary(updateSummaryDatas[0] || {
+                id: "",
+                start_time: "",
+                end_time: "",
+                duration: "",
+                date_created: new Date(),
+                date_updated: new Date(),
+                inserted_pharmacies: 0,
+                updated_pharmacies: 0,
+                skipped_pharmacies: 0,
+                already_open_pharmacies: 0,
+                mode: "manual",
+            })
 
             console.log("lineChartDatas", lineChartDatas)
             console.log("reviewChartDatas", reviewChartDatas)
             console.log("stateChartDatas", stateChartDatas)
             console.log("ActivitiesDatas", ActivitiesDatas)
+            console.log("updateSummary", updateSummaryDatas[0])
 
         } catch (error) {
             console.log(error)
@@ -95,7 +144,9 @@ export const DashboardContextProvider = ({ children }: any) => {
             stateChartDatas,
             setStateChartDatas,
             activities,
-            setActivities
+            setActivities,
+            updateSummary,
+            setUpdateSummary
         }
     }, [
         isLoading,
@@ -107,7 +158,9 @@ export const DashboardContextProvider = ({ children }: any) => {
         stateChartDatas,
         setStateChartDatas,
         activities,
-        setActivities
+        setActivities,
+        updateSummary,
+        setUpdateSummary
     ])
     return (
         <DashboardContext.Provider value={value}>
