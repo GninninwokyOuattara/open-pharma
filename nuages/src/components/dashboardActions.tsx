c125153a32c2edc86195bf9c4cefdd4ecb901d86
g-ouattara <idrissgouattara@gmail.com>
2023-05-09 23:08:21 +0000

    refresh pharmacies button is working

--- nuages/src/components/dashboardActions.tsx
+++ nuages/src/components/dashboardActions.tsx
@@ -1,10 +1,16 @@
 import { Button, Icon, VStack } from "@chakra-ui/react"
+import { useContext } from "react"
 import { BiRefresh } from "react-icons/bi"
 import { MdSettings } from "react-icons/md"
 import { palette } from "../colorPalette"
+import { DashboardContext, DashboardContextInterface } from "../contexts/dashboardContext"
 
 
 export const DashboardActions = () => {
+
+    const { getStatistics, isLoading } = useContext(DashboardContext) as DashboardContextInterface
+
+
     return (
         <VStack
             height={"100%"}
@@ -21,13 +27,15 @@ export const DashboardActions = () => {
             <ActionButton
                 title={"Refresh Dashboard"}
                 icon={<Icon as={BiRefresh} />}
-                onClick={() => console.log("clicked")}
+                onClick={() => getStatistics()}
+                isLoading={isLoading}
             />
 
             <ActionButton
                 title={"Trigger Data Actualization"}
                 icon={<Icon as={MdSettings} />}
                 onClick={() => console.log("clicked")}
+                isLoading={isLoading}
             />
 
 
@@ -42,10 +50,11 @@ export const DashboardActions = () => {
 interface ActionButtonProps {
     title: string,
     icon: any,
-    onClick: () => void
+    onClick: () => void,
+    isLoading: boolean
 }
 
-const ActionButton = ({ title, icon, onClick }: ActionButtonProps) => {
+const ActionButton = ({ title, icon, onClick, isLoading }: ActionButtonProps) => {
     return (
         <Button
             width={"100%"}
@@ -56,6 +65,7 @@ const ActionButton = ({ title, icon, onClick }: ActionButtonProps) => {
             color={"white"}
             _hover={{ backgroundColor: "orange.500" }}
             onClick={onClick}
+            isLoading={isLoading}
         // isLoading
         >
             {title}
