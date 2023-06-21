1a135d726def43c805a1f7d0da0e25b94a2c9283
g-ouattara <idrissgouattara@gmail.com>
2023-06-21 22:01:23 +0000

    new buttons mode review

--- nuages/src/components/actionButtons.tsx
+++ nuages/src/components/actionButtons.tsx
@@ -1,15 +1,18 @@
 import { IconButton } from "@chakra-ui/react"
+import { FaClipboardList } from "react-icons/fa"
 import { FiCheck, FiX } from "react-icons/fi"
 import { TbPlugConnected } from "react-icons/tb"
 
 interface ReviewButtonProps {
     onClick: () => void
-    for: "validate" | "invalidate" | "link"
+    for: "validate" | "invalidate" | "link" | "review"
     // rowIsLoading: boolean
 }
 
 export const ReviewButton: React.FC<ReviewButtonProps> = (props) => {
-    const icon = props.for === "validate" ? <FiCheck /> : props.for === "invalidate" ? <FiX /> : <TbPlugConnected />
+    const icon = props.for === "validate" ? <FiCheck />
+        : props.for === "invalidate" ? <FiX />
+            : props.for === "review" ? <FaClipboardList /> : <TbPlugConnected />
     return (
         <IconButton
             aria-label={props.for}
