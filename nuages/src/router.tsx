3fdb1fcc889fa88d3fa1602a1be5c5ff047d7288
g-ouattara <idrissgouattara@gmail.com>
2023-07-06 23:30:59 +0000

    change the way the createBrowserRouter is used

--- nuages/src/router.tsx
+++ nuages/src/router.tsx
@@ -1,15 +1,17 @@
 import {
-    createBrowserRouter, redirect
+    Route,
+    Routes,
+    createBrowserRouter
 } from "react-router-dom";
-import Layout from "./components/layout";
 
 // import Home
 import PendingReviewsPage from "./pages/pendingReviewsPage";
 
+import Layout from "./components/layout";
 import { EditModalContextProvider } from "./contexts/EditModalContext";
 import { EditPendingModalContextProvider } from "./contexts/EditPendingModalContext";
 import { DashboardContextProvider } from "./contexts/dashboardContext";
-import { PharmaciesContextProvider } from './contexts/pharmaciesContext';
+import { PharmaciesContextProvider } from "./contexts/pharmaciesContext";
 import { PharmaciesReviewContextProvider } from "./contexts/pharmaciesReviewContext";
 import Dashboard from "./pages/dashboard";
 import LoginPage from "./pages/loginPage";
@@ -22,58 +24,96 @@ import PharmaciesPage from "./pages/pharmaciesPage";
 // - Pharmacies
 // - Pending Reviews
 
+
+const RootRouting = () => {
+
+    return (
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path="/" element={<Layout />}>
+                <Route path="/dashboard" element={<DashboardContextProvider>
+                    <Dashboard />
+                </DashboardContextProvider>} />
+                <Route path="/pharmacies" element={<PharmaciesContextProvider>
+                    <EditModalContextProvider>
+                        <PharmaciesPage />
+                    </EditModalContextProvider>
+
+                </PharmaciesContextProvider>} />
+                <Route path="/pending-reviews" element={<PharmaciesReviewContextProvider>
+                    <EditPendingModalContextProvider>
+                        <PendingReviewsPage />
+                    </EditPendingModalContextProvider>
+                </PharmaciesReviewContextProvider>} />
+            </Route>
+        </Routes>
+    )
+}
+
+
 const appRouting = createBrowserRouter([
     {
-        path: "/",
-        element: <Layout />,
-        loader: () => {
-            if (1) {
-                console.log("Redirecting to login page")
-                return redirect("/login");
-            }
-        },
-
-
-        children: [
-
-            {
-                path: "/dashboard",
-
-                element: <DashboardContextProvider>
-                    <Dashboard />
-                </DashboardContextProvider>
-            },
-            {
-                path: "/pharmacies",
-                element:
-                    <PharmaciesContextProvider>
-                        <EditModalContextProvider>
-                            <PharmaciesPage />
-                        </EditModalContextProvider>
-
-                    </PharmaciesContextProvider>
-
-            },
-            {
-                path: "/pending-reviews",
-                element:
-                    <PharmaciesReviewContextProvider>
-                        <EditPendingModalContextProvider>
-                            <PendingReviewsPage />
-                        </EditPendingModalContextProvider>
-                    </PharmaciesReviewContextProvider>,
-            }
-
-        ]
-    },
-    {
-        path: "/login",
-        element: <LoginPage />
+        path: "*",
+        element: <RootRouting />,
     }
+])
 
 
 
-]);
+// const appRouting = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Layout />,
+//         // loader: () => {
+//         //     if (1) {
+//         //         console.log("Redirecting to login page")
+//         //         return redirect("/login");
+//         //     }
+//         // },
+
+
+//         children: [
+
+//             {
+//                 path: "/dashboard",
+
+//                 element: <DashboardContextProvider>
+//                     <Dashboard />
+//                 </DashboardContextProvider>
+//             },
+//             {
+//                 path: "/pharmacies",
+//                 element:
+//                     <PharmaciesContextProvider>
+//                         <EditModalContextProvider>
+//                             <PharmaciesPage />
+//                         </EditModalContextProvider>
+
+//                     </PharmaciesContextProvider>
+
+//             },
+//             {
+//                 path: "/pending-reviews",
+//                 element:
+//                     <PharmaciesReviewContextProvider>
+//                         <EditPendingModalContextProvider>
+//                             <PendingReviewsPage />
+//                         </EditPendingModalContextProvider>
+//                     </PharmaciesReviewContextProvider>,
+//             }
+
+//         ]
+//     },
+//     {
+//         path: "/login",
+//         element: <LoginPage />
+//     }
+
+
+
+// ]);
 
 
 export default appRouting;
