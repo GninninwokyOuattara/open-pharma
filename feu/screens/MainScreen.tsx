67017e31c1e26a3dbe82a67bd69727bee0e61234
g-ouattara <idrissgouattara@gmail.com>
2023-07-16 00:55:50 +0000

    clean it up a little

--- feu/screens/MainScreen.tsx
+++ feu/screens/MainScreen.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import BottomBar from "../components/screens-components/BottomBar";
-import { RootReducerType } from "../types/dataTypes";
 
 
 import MainBottomSheet from "../components/screens-components/BottomSheet";
 import { getOpenPharmaPharmaciesDatas } from "../stores/pharmaciesActions";
 
-import * as Location from "expo-location";
 import ToolBar from "../components/ToolBar";
 import AppMap from "../components/screens-components/AppMap";
 
@@ -20,26 +18,10 @@ const MainScreen = () => {
 
     const dispatch = useDispatch();
 
-    const isLococationPermissionGranted = useSelector((state: RootReducerType) => {
-        return state.pharmacies.isLocationPermissionGranted;
-    });
-
 
 
     React.useEffect(() => {
-
-        if (isLococationPermissionGranted) {
-            Location.getCurrentPositionAsync({}).then((location) => {
-                dispatch(getOpenPharmaPharmaciesDatas(location))
-
-            });
-
-        } else {
-            dispatch(getOpenPharmaPharmaciesDatas(null))
-        }
-
-
-
+        dispatch(getOpenPharmaPharmaciesDatas(null));
     }, [])
 
 
@@ -54,8 +36,6 @@ const MainScreen = () => {
                 flex: 1,
             }}
         >
-
-
             <AppMap />
             <MainBottomSheet />
             <BottomBar />
