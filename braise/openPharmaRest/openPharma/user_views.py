9a9c7113af40583f7c8bdc2742cb137716c19947
g-ouattara <idrissgouattara@gmail.com>
2023-07-29 17:46:44 +0000

    two serliazers for list and details cases

--- braise/openPharmaRest/openPharma/user_views.py
+++ braise/openPharmaRest/openPharma/user_views.py
@@ -1,7 +1,8 @@
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from openPharma.users_serializers import UserPharmaciesSerializer
+from openPharma.users_serializers import (UserPharmaciesSerializer,
+                                          UsersPharmacieDetailsSerializer)
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -12,6 +13,11 @@ from .models import Pharmacy
 class UsersPharmaciesViewset(ReadOnlyModelViewSet):
     serializer_class = UserPharmaciesSerializer
     queryset = Pharmacy.objects.filter(active=True, pending_review=False)
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return UsersPharmacieDetailsSerializer
+        return UserPharmaciesSerializer
 
     @method_decorator(cache_page(60*2))
     def dispatch(self, request, *args, **kwargs):
