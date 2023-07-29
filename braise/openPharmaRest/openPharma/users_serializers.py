439096d48712c0f18583bac5db8df98eb87da2a0
g-ouattara <idrissgouattara@gmail.com>
2023-07-29 17:47:22 +0000

    add serializers for details

--- braise/openPharmaRest/openPharma/users_serializers.py
+++ braise/openPharmaRest/openPharma/users_serializers.py
@@ -4,16 +4,35 @@ from openPharma.models import OpenPharmacy, Pharmacy
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 
-class UserPharmaciesSerializer(ModelSerializer):
+class PharmacyOpenState(ModelSerializer):
     open = SerializerMethodField()
 
-    class Meta:
-        model = Pharmacy
-        fields = ["id", "name", "coordinates", "open"]
-
     def get_open(self, obj):
+        """
+        Check if a pharmacy is currently open.
+
+        Args:
+            obj (Pharmacy): The pharmacy object to check.
+
+        Returns:
+            bool: True if the pharmacy is open, False otherwise.
+        """
         currente_date = timezone.now()
         date = currente_date.strftime("%Y-%m-%d")
         if OpenPharmacy.objects.filter(pharmacy=obj, open_from__lte=currente_date, open_until__gte=currente_date):
             return True
         return False
+
+
+class UserPharmaciesSerializer(PharmacyOpenState):
+
+    class Meta:
+        model = Pharmacy
+        fields = ["id", "name", "coordinates"]
+
+
+class UsersPharmacieDetailsSerializer(PharmacyOpenState):
+    class Meta:
+        model = Pharmacy
+        fields = ("id", "name", "zone", "description", "director",
+                  "addresses", "phones", "coordinates", "open")
