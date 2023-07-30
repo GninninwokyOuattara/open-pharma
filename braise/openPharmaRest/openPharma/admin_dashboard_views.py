07180618db0bc64468f7a3ff96717b322801db37
g-ouattara <idrissgouattara@gmail.com>
2023-07-30 17:06:50 +0000

    ReviewsStatesCountView and StatesCountView

--- braise/openPharmaRest/openPharma/admin_dashboard_views.py
+++ braise/openPharmaRest/openPharma/admin_dashboard_views.py
@@ -2,14 +2,15 @@
 
 from django.db.models import Count
 from django.db.models.functions import TruncWeek
-from openPharma.models import Pharmacy
+from django.utils.timezone import now
+from openPharma.models import OpenPharmacy, Pharmacy
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from openPharmaRest.authorization import IsAuthenticated
 
 
-class PharmaciesOverWeeksViewset(IsAuthenticated, ReadOnlyModelViewSet):
+class PharmaciesOverWeeksCountViews(IsAuthenticated, ReadOnlyModelViewSet):
 
     http_method_names = ['get']
 
@@ -32,7 +33,65 @@ class PharmaciesOverWeeksViewset(IsAuthenticated, ReadOnlyModelViewSet):
         return Response(data)
 
 
-# get_pharmacies_states
+class PharmaciesStatesCountView(IsAuthenticated, ReadOnlyModelViewSet):
+
+    http_method_names = ['get']
+
+    queryset = Pharmacy.objects.annotate(
+        count=Count('id')).order_by('count')
+
+    def list(self, request, *args, **kwargs):
+        current_date = now()
+        active_pharmacies_count = Pharmacy.objects.filter(active=True).count()
+        inactive_Pharmacies_count = Pharmacy.objects.filter(
+            active=False).count()
+        open_pharmacies_count = OpenPharmacy.objects.filter(
+            open_from__lte=current_date, open_until__gte=current_date).count()
+
+        data = [
+            {
+                "name": "Active Pharmacies",
+                "count": active_pharmacies_count,
+                "fill": "#3182CE",
+            },
+
+            {
+                "name": "Inactive Pharmacies",
+                "count": inactive_Pharmacies_count,
+                "fill": "#718096",
+            },
+            {
+                "name": "Open Pharmacies",
+                "count": open_pharmacies_count,
+                "fill": "#38A169",
+            }
+        ]
+
+        return Response(data)
 
 
-# get_reviews_states
+class PharmaciesReviewsStatesCountView(IsAuthenticated, ReadOnlyModelViewSet):
+
+    http_method_names = ['get']
+
+    def list(self, request, *args, **kwargs):
+        pending_review_pharmacies_count = Pharmacy.objects.filter(
+            pending_review=True).count()
+        reviewed_pharmacies_count = Pharmacy.objects.filter(
+            pending_review=False).count()
+
+        data = [
+            {
+                "name": "Pharmacies Pending Review",
+                "count": pending_review_pharmacies_count,
+                "fill": "#718096",
+            },
+
+            {
+                "name": "Pharmacies Reviewed",
+                "count": reviewed_pharmacies_count,
+                "fill": "#3182CE",
+            }
+        ]
+
+        return Response(data)
