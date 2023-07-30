f70b27f02e48d96a172afbdcbad8957995c4c040
g-ouattara <idrissgouattara@gmail.com>
2023-07-30 15:01:01 +0000

    patch way the key

--- braise/openPharmaRest/openPharma/admin_serializers.py
+++ braise/openPharmaRest/openPharma/admin_serializers.py
@@ -54,29 +54,4 @@ class PharmacieDetailsSerializer(BaseAdminSerializer):
     class Meta:
         model = Pharmacy
         fields = ("id", "name", "zone", "director", "addresses", "phones",
-                  "description", "coordinates", "open", "date_created")
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.zone = validated_data.get('zone', instance.zone)
-        instance.director = validated_data.get('director', instance.director)
-        instance.addresses = validated_data.get(
-            'addresses', instance.addresses)
-        instance.phones = validated_data.get('phones', instance.phones)
-        instance.email = validated_data.get('email', instance.email)
-        instance.website = validated_data.get('website', instance.website)
-        instance.description = validated_data.get(
-            'description', instance.description)
-        instance.images = validated_data.get('images', instance.images)
-        instance.google_maps_link = validated_data.get(
-            'google_maps_link', instance.google_maps_link)
-        instance.latitude = validated_data.get('latitude', instance.latitude)
-        instance.longitude = validated_data.get(
-            'longitude', instance.longitude)
-
-        instance.active = validated_data.get('active', instance.active)
-        instance.pending_review = validated_data.get(
-            'pending_review', instance.pending_review)
-        instance.save()
-
-        return instance
+                  "description", "latitude", "longitude", "coordinates", "open", "date_created")
