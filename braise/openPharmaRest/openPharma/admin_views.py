4bb059eaaa40b86e36c6bbbef08e1a26cf16703a
g-ouattara <idrissgouattara@gmail.com>
2023-07-30 15:31:07 +0000

    view for pending-review pharmacies

--- braise/openPharmaRest/openPharma/admin_views.py
+++ braise/openPharmaRest/openPharma/admin_views.py
@@ -2,6 +2,7 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from openPharma.admin_serializers import (PharmacieDetailsSerializer,
+                                          PharmaciesPendingReviewSerializer,
                                           PharmaciesSerializer)
 from openPharma.models import Pharmacy
 from rest_framework.decorators import action
@@ -14,6 +15,7 @@ from openPharmaRest.panigation import ResultsSetPagination
 class PharmaciesAsAdminViewset(ModelViewSetWithAuthorization, ResultsSetPagination):
     queryset = Pharmacy.objects.all()
     # serializer_class = PharmaciesSerializer
+    http_method_names = ["get", "post", "patch"]
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
@@ -65,3 +67,29 @@ class PharmaciesAsAdminViewset(ModelViewSetWithAuthorization, ResultsSetPaginati
             return Response({"message": message, "data": data}, status=200)
         except Exception as error:
             return Response({"message": "An unexpected error occured"}, status=500)
+
+
+class PharmaciesPendingReviewAsAdminViewset(ModelViewSetWithAuthorization, ResultsSetPagination):
+    queryset = Pharmacy.objects.filter(pending_review=True)
+    serializer_class = PharmaciesSerializer
+    http_method_names = ["get", "post"]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return PharmacieDetailsSerializer
+        return PharmaciesPendingReviewSerializer
+
+    def list(self, request, *args, **kwargs):
+
+        name = request.query_params.get("name") or ""
+        zone = request.query_params.get("zone") or ""
+
+        queryset = Pharmacy.objects.filter(
+            name__icontains=name,
+            # zone__icontains=zone,
+            pending_review=True
+        )
+        page = self.paginate_queryset(queryset, request)
+        serializer = PharmaciesPendingReviewSerializer(page, many=True)
+
+        return self.get_paginated_response(serializer.data)
