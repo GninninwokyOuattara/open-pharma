6a61c099c46f31dcf1116fd8d4c67fd2a9473928
g-ouattara <idrissgouattara@gmail.com>
2023-07-30 21:39:37 +0000

    banished

--- braise/openPharmaRest/openTracker/models.py
+++ braise/openPharmaRest/openTracker/models.py
@@ -1,34 +1,34 @@
-import uuid
+# import uuid
 
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
 
 
-class TrackerHistory(models.Model):
-    class Meta:
-        db_table = 'tracker_history'
+# class TrackerHistory(models.Model):
+#     class Meta:
+#         db_table = 'tracker_history'
 
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    duration = models.DurationField()
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now=True)
-    # collected_data = models.JSONField()
-    inserted_pharmacies = models.IntegerField(
-        blank=True, null=True, default=0
-    )
-    updated_pharmacies = models.IntegerField(
-        blank=True, null=True, default=0
+#     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+#     start_time = models.DateTimeField()
+#     end_time = models.DateTimeField()
+#     duration = models.DurationField()
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     date_updated = models.DateTimeField(auto_now=True)
+#     # collected_data = models.JSONField()
+#     inserted_pharmacies = models.IntegerField(
+#         blank=True, null=True, default=0
+#     )
+#     updated_pharmacies = models.IntegerField(
+#         blank=True, null=True, default=0
 
-    )
-    skipped_pharmacies = models.IntegerField(
-        blank=True, null=True, default=0
+#     )
+#     skipped_pharmacies = models.IntegerField(
+#         blank=True, null=True, default=0
 
-    )
-    already_open_pharmacies = models.IntegerField(
-        blank=True, null=True, default=0
-    )
-    # field 'mode' which is either 'manual' or 'automatic'
-    mode = models.CharField(max_length=10)
+#     )
+#     already_open_pharmacies = models.IntegerField(
+#         blank=True, null=True, default=0
+#     )
+#     # field 'mode' which is either 'manual' or 'automatic'
+#     mode = models.CharField(max_length=10)
--- braise/openPharmaRest/openTracker/serializers.py
+++ braise/openPharmaRest/openTracker/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers
+# from rest_framework import serializers
 
-from openTracker.models import TrackerHistory
+# from openTracker.models import TrackerHistory
 
 
-class TrackerHistoryListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TrackerHistory
-        fields = "__all__"
+# class TrackerHistoryListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = TrackerHistory
+#         fields = "__all__"
