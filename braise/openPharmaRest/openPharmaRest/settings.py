4259a9bf0446d26450d72e53cc354045920e28e4
g-ouattara <idrissgouattara@gmail.com>
2023-07-30 11:55:10 +0000

    no more global pagination

--- braise/openPharmaRest/openPharmaRest/settings.py
+++ braise/openPharmaRest/openPharmaRest/settings.py
@@ -70,8 +70,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 25
 
 }
 
