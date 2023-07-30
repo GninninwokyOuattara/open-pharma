bd7fc42ec415ccee96e751cf806a854e3067ced1
g-ouattara <idrissgouattara@gmail.com>
2023-07-30 12:19:42 +0000

    activation and deactivation actions

--- braise/openPharmaRest/openPharma/admin_views.py
+++ braise/openPharmaRest/openPharma/admin_views.py
@@ -4,6 +4,8 @@ from django.views.decorators.cache import cache_page
 from openPharma.admin_serializers import (PharmacieDetailsSerializer,
                                           PharmaciesSerializer)
 from openPharma.models import Pharmacy
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from openPharmaRest.authorization import ModelViewSetWithAuthorization
 from openPharmaRest.panigation import ResultsSetPagination
@@ -33,3 +35,31 @@ class PharmaciesAsAdminViewset(ModelViewSetWithAuthorization, ResultsSetPaginati
         serializer = PharmaciesSerializer(page, many=True)
 
         return self.get_paginated_response(serializer.data)
+
+    @action(detail=True, methods=['post'])
+    def activate(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.active = True
+            instance.save()
+            serializer = self.get_serializer(instance)
+
+            message = f"{instance.name} has been activated succesfully"
+            data = serializer.data
+            return Response({"message": message, "data": data}, status=200)
+        except Exception as error:
+            return Response({"message": "An unexpected error occured"}, status=500)
+
+    @action(detail=True, methods=['post'])
+    def deactivate(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance.active = False
+            instance.save()
+            serializer = self.get_serializer(instance)
+
+            message = f"{instance.name} has been deactivated."
+            data = serializer.data
+            return Response({"message": message, "data": data}, status=200)
+        except Exception as error:
+            return Response({"message": "An unexpected error occured"}, status=500)
