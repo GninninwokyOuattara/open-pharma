c9df7a140a63f91d3d2a7ce32eb023b2708cc69d
g-ouattara <idrissgouattara@gmail.com>
2023-08-19 20:16:22 +0000

    test query

--- nuages/src/contexts/pharmaciesReviewContext.tsx
+++ nuages/src/contexts/pharmaciesReviewContext.tsx
@@ -1,4 +1,7 @@
 import { createContext, useMemo } from "react";
+import { useQuery } from "react-query";
+import usePharmaciesPendingReviewQueries from "../hooks/usePharmaciesPendingReviewQueries";
+import { PharmacyBaseInfo } from "../types";
 
 const backendUrl = import.meta.env.VITE_APP_DJANGO_API_URL;
 
@@ -10,18 +13,30 @@ export interface PharmaciesReviewContextInterface {
 }
 
 
+interface QueryData {
+    data: {
+        count: number,
+        previous: null | string,
+        next: null | string
+        results: PharmacyBaseInfo[]
+    }
+}
+
+
 export const PharmaciesReviewContext = createContext<PharmaciesReviewContextInterface | null>(null);
 
 
 export const PharmaciesReviewContextProvider = ({ children }: any) => {
 
 
+    const { getPharmaciesPendingReview } = usePharmaciesPendingReviewQueries();
+
+    const { isLoading, data } = useQuery<QueryData, any>("pharmaciesPendingReview", getPharmaciesPendingReview);
+
+
     const contextValue = useMemo(() => ({
-
-    }), [
-
-
-    ]);
+        pharmaciesPendingReview: data?.data.results
+    }), [data]);
 
 
 
