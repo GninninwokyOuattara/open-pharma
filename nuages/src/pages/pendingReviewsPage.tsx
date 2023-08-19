252e0833862fcb4ca1012b74ff4609c32ec81000
g-ouattara <idrissgouattara@gmail.com>
2023-08-19 18:07:40 +0000

    cleanup pending review page

--- nuages/src/pages/pendingReviewsPage.tsx
+++ nuages/src/pages/pendingReviewsPage.tsx
@@ -3,34 +3,14 @@
 
 
 
-
-// import FiRefreshCcw from react-icons
-
-import { Box, HStack, VStack } from "@chakra-ui/react";
-import { useContext, useEffect } from "react";
-import { palette } from "../colorPalette";
-import RefreshButton from "../components/refreshButton";
-import SearchBar from "../components/searchBar";
-import { PharmaciesReviewContext, PharmaciesReviewContextInterface } from "../contexts/pharmaciesReviewContext";
+import { Text, VStack } from "@chakra-ui/react";
 
 
-import EditPendingPharmacyModal from "../components/editPendingPharmacyModal";
-import { PendingReviewPageTable } from "../components/pendingReviewTable";
-import EditPendingModalContext, { EditPendingModalContextInterface } from "../contexts/EditPendingModalContext";
 
 
 
 
 const PendingReviewsPage = () => {
-
-    // Hooks
-    const { refreshDatas } = useContext(PharmaciesReviewContext) as PharmaciesReviewContextInterface
-
-    useEffect(() => {
-        console.log("fired for pending review page")
-        refreshDatas()
-    }, [])
-
 
 
     return (
@@ -40,10 +20,7 @@ const PendingReviewsPage = () => {
                 height={"full"}
                 width={"full"}
             >
-
-                <PendingReviewPageHeader />
-                <PendingReviewPageTable />
-
+                <Text>Hello World</Text>
             </VStack>
         </>
     )
@@ -52,38 +29,7 @@ const PendingReviewsPage = () => {
 
 
 
-const PendingReviewPageHeader = () => {
 
-    const { refreshDatas, setSearch, handleSearch } = useContext(PharmaciesReviewContext) as PharmaciesReviewContextInterface
-
-    const { isOpen, onOpen, onClose } = useContext(EditPendingModalContext) as EditPendingModalContextInterface
-
-    return (
-        <Box
-            height={"100px"}
-            width={"100%"}
-            // borderTop={"1px solid"}
-            borderBottom={"1px solid "}
-            borderColor={palette.custom.niceOrange}
-            display={"flex"}
-            alignItems={"center"}
-            paddingX={2}
-            zIndex={10}
-            justifyContent={"space-between"}
-        >
-            <HStack >
-
-                <RefreshButton isLoading={false} onClick={() => refreshDatas()} />
-            </HStack>
-            <SearchBar onChange={handleSearch} />
-            <EditPendingPharmacyModal isOpen={false} onClose={function (): void {
-                throw new Error("Function not implemented.");
-            }} />
-
-        </Box>
-    )
-
-}
 
 
 
