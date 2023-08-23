ad5f29902e50d5bc55074b75db5c4e3a72e7e936
g-ouattara <idrissgouattara@gmail.com>
2023-08-23 22:42:31 +0000

    restyle navigation bar

--- nuageux/src/components/navigation/NavigationBar.tsx
+++ nuageux/src/components/navigation/NavigationBar.tsx
@@ -1,5 +1,7 @@
+import { Separator } from "@/components/ui/separator";
 import { AiFillPieChart } from 'react-icons/ai';
 import { GiChoice } from "react-icons/gi";
+import { IoLogOutOutline } from "react-icons/io5";
 import { RiFileList3Fill } from "react-icons/ri";
 
 
@@ -8,23 +10,26 @@ import NavigationBarItem from './NavigationBarItem';
 const NavigationBar = () => {
     return (
         <div
-            className="h-14 w-full flex flex-row border-b-2  items-center p-1"
+            className="h-14 w-full flex flex-row border-b-2  items-center p-1 border-2 justify-between"
         >
             <div className="w-32 font-bold text-center">OpenPharma</div>
 
-            <div className="w-full h-full flex items-center justify-center">
+            <div className="h-full flex items-center justify-center">
 
-                <div className="border-2 rounded-md px-5 h-full flex flex-row gap-x-4 items-center bg-appPrimary">
-                    <NavigationBarItem to={"/dashboard"} icon={<AiFillPieChart size={20} />} />
-                    <div className="w-0.5 h-4/5 bg-gray-300" />
-                    <NavigationBarItem to={"/pharmacies"} icon={<RiFileList3Fill size={20} />} />
-                    <div className="w-0.5 h-4/5 bg-gray-300" />
+                <div className="px-5 h-full flex flex-row gap-x-4 items-center ">
+                    <NavigationBarItem to={"/dashboard"} icon={<AiFillPieChart size={25} />} />
+                    <Separator orientation='vertical' />
+                    <NavigationBarItem to={"/pharmacies"} icon={<RiFileList3Fill size={25} />} />
+                    <Separator orientation='vertical' />
+                    <NavigationBarItem to="/reviews" icon={<GiChoice size={25} />} />
+                    <Separator orientation='vertical' />
+                    <NavigationBarItem to="/" icon={<IoLogOutOutline size={25} />} />
 
-                    <NavigationBarItem to="/reviews" icon={<GiChoice size={20} />} />
+
                 </div>
 
             </div>
-            <div className="w-32 text-center font-bold">logout</div>
+            {/* <div className="w-32 text-center font-bold">logout</div> */}
 
             {/* <div className="w-30">Logout</div> */}
 
