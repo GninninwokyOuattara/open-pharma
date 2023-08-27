e1eb2c46f6e00ac7afc166f0dd6c42fa21770083
g-ouattara <idrissgouattara@gmail.com>
2023-08-27 23:40:11 +0000

    pharmacies page

--- nuageux/src/pages/pharmacies.tsx
+++ nuageux/src/pages/pharmacies.tsx
@@ -1,8 +1,55 @@
-import withNavigationBarLayout from "@/components/layout/withNavigationBarLayout"
+import { getPharmacies } from "@/api/pharmaciesApis";
+import DataTable from "@/components/datatables/datatable";
+import { columns } from "@/components/datatables/pharmaciesDatatableColumns";
+import TablePagination from "@/components/datatables/tablePagination";
+
+import withNavigationBarLayout from "@/components/layout/withNavigationBarLayout";
+import { Input } from "@/components/ui/input";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 function pharmacies() {
+    const [nameFilter, setNameFilter] = useState("")
+    const [page, setPage] = useState(1)
+    const [zoneFilter, setZoneFilter] = useState("")
+
+    const { isLoading, isError, data, isSuccess } = useQuery({
+        queryKey: ['pending-reviews-pharmacies', nameFilter, page],
+        queryFn: () => getPharmacies({
+            page: page,
+            nameFilter: nameFilter,
+            zoneFilter: zoneFilter
+        }),
+        keepPreviousData: true
+    })
+
+
     return (
-        <div>pharmacies</div>
+        <div className="px-4 pt-4">
+
+            <Input placeholder="Search pharmacies"
+            // value={nameFilter}
+            // onChange={setFilterWithDelay}
+            />
+            <div className="mt-4">
+
+
+                <DataTable columns={columns} data={data?.data.results || []} />
+
+
+                <div className="my-4 flex justify-center align-center ">
+
+                    <TablePagination
+                        count={data?.data.count || 0}
+                        page={page}
+                        pageLength={25}
+                        setPageFn={setPage}
+                        next={data?.data.next || null}
+                        previous={data?.data.previous || null}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
