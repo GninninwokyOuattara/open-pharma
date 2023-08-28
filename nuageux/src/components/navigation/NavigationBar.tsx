39009b1061d670db3828c238b63b7a6889ad1879
g-ouattara <idrissgouattara@gmail.com>
2023-08-28 22:29:02 +0000

    removed separator

--- nuageux/src/components/navigation/NavigationBar.tsx
+++ nuageux/src/components/navigation/NavigationBar.tsx
@@ -1,4 +1,3 @@
-import { Separator } from "@/components/ui/separator";
 import { AiFillPieChart } from 'react-icons/ai';
 import { GiChoice } from "react-icons/gi";
 import { IoLogOutOutline } from "react-icons/io5";
@@ -10,7 +9,7 @@ import NavigationBarItem from './NavigationBarItem';
 const NavigationBar = () => {
     return (
         <div
-            className="h-14 w-full flex flex-row border-b-2  items-center p-1 border-appPrimary justify-between sticky top-0 left-0 bg-white z-50"
+            className="h-14 w-full flex flex-row border-b-2  items-center p-1 border-appPrimary justify-between sticky top-0 left-0 bg-appPrimary z-50"
         >
             <div className="w-32 font-bold text-center text-xl ">Open<span className="text-green-500">Pharma</span></div>
 
@@ -18,11 +17,8 @@ const NavigationBar = () => {
 
                 <div className="px-5 h-full flex flex-row gap-x-4 items-center ">
                     <NavigationBarItem to={"/dashboard"} icon={AiFillPieChart} />
-                    <Separator orientation='vertical' className="bg-appPrimary" />
                     <NavigationBarItem to={"/pharmacies"} icon={RiFileList3Fill} />
-                    <Separator orientation='vertical' className="bg-appPrimary" />
                     <NavigationBarItem to="/reviews" icon={GiChoice} />
-                    <Separator orientation='vertical' className="bg-appPrimary" />
                     <NavigationBarItem
                         to="/"
                         icon={IoLogOutOutline}
