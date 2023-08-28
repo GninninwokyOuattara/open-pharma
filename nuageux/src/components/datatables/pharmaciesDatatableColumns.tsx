7abd260b122ad12c385d30e0525bae4f5414f872
g-ouattara <idrissgouattara@gmail.com>
2023-08-28 00:35:46 +0000

    switch handler

--- nuageux/src/components/datatables/pharmaciesDatatableColumns.tsx
+++ nuageux/src/components/datatables/pharmaciesDatatableColumns.tsx
@@ -1,6 +1,9 @@
+import { activatePharmacy, deactivatePharmacy } from "@/api/pharmaciesApis"
 import { Switch } from "@/components/ui/switch"
 import { PharmacyData } from "@/types/dataTypes"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { ColumnDef } from "@tanstack/react-table"
+import { useState } from "react"
 import PharmaciesTableRow from "../pharmaciesTableRow"
 
 
@@ -30,11 +33,54 @@ export const columns: ColumnDef<PharmacyData>[] = [
         accessorKey: "active",
         header: "Status",
         cell: ({ row }) => {
-            return <Switch
-                checked={row.original.active}
-                onCheckedChange={() => { }}
-                aria-readonly
-            />
+
+            const [toggleForm, setToggleForm] = useState<"activation" | "deactivation">(() => {
+                return row.original.active ? "deactivation" : "activation"
+            })
+            const queryClient = useQueryClient();
+
+            const activationMutation = useMutation({
+                mutationFn: () => activatePharmacy(row.original.id),
+                onSuccess: () => {
+                    // Invalidate and refetch
+                    setToggleForm("deactivation")
+                    queryClient.invalidateQueries({ queryKey: ["pharmacies"] })
+                },
+                onError: () => {
+                    console.log("Well Well Well it seems like something went wrong")
+                }
+            })
+
+            const deactivationMutation = useMutation({
+                mutationFn: () => deactivatePharmacy(row.original.id),
+                onSuccess: () => {
+                    // Invalidate and refetch
+                    setToggleForm("activation")
+                    queryClient.invalidateQueries({ queryKey: ["pharmacies"] })
+                },
+                onError: () => {
+                    console.log("Well Well Well it seems like something went wrong")
+                }
+            })
+
+            const switcher = () => {
+                if (toggleForm === "activation") {
+                    activationMutation.mutate()
+                } else {
+                    deactivationMutation.mutate()
+                }
+            }
+
+            return (
+                <>
+                    <Switch
+                        className="bg-red-500 text-red-500"
+                        checked={row.original.active}
+                        onCheckedChange={switcher}
+                        aria-readonly
+                    />
+
+                </>)
         }
     }
 ]
