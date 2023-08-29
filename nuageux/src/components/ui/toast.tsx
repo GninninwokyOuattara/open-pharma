427f8bae3f11cb9d4a1802771d265b35a71a11a0
g-ouattara <idrissgouattara@gmail.com>
2023-08-29 18:06:05 +0000

    og

--- nuageux/src/components/ui/toast.tsx
+++ nuageux/src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-14 w-full flex-col-reverse sm:bottom-0   sm:flex-col inset-x-0 mx-auto max-w-[1280px] justify-center",
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
