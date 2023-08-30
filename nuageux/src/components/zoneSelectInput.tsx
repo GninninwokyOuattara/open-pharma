b0a1aa1fbe15edad183ecf22f0d87e243aac5221
g-ouattara <idrissgouattara@gmail.com>
2023-08-30 00:36:06 +0000

    sexy select component

---
+++ nuageux/src/components/zoneSelectInput.tsx
@@ -0,0 +1,68 @@
+
+import { getZones } from "@/api/pharmaciesApis"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useQuery } from "@tanstack/react-query"
+
+
+interface ZoneSelectInputProps {
+    selectFn: React.Dispatch<React.SetStateAction<string>>
+}
+
+
+const ZoneSelectInput: React.FC<ZoneSelectInputProps> = ({ selectFn }) => {
+
+    const { isLoading, data } = useQuery({
+        queryKey: ['get-zones'],
+        queryFn: () => getZones()
+    })
+
+
+    return (
+        <Select
+            disabled={isLoading}
+            onValueChange={(value) => {
+                if (value === "*") {
+                    return selectFn("")
+                }
+                return selectFn(value)
+            }}
+        >
+            <SelectTrigger
+                className="w-full md:w-64"
+            >
+                <SelectValue placeholder="Toutes les zones" />
+            </SelectTrigger>
+            <SelectContent className="h-64">
+                <SelectItem
+                    key={"g1"}
+                    value={"*"}
+                    className="h-12"
+                >
+                    {"Toutes les zones"}
+                </SelectItem>
+                {
+                    data?.data.map((zone, idx) => (
+                        <SelectItem
+                            key={idx}
+                            value={zone}
+                            className="h-12"
+                        >
+                            {zone}
+                        </SelectItem>
+                    ))
+                }
+
+            </SelectContent>
+
+        </Select>
+
+    )
+}
+
+export default ZoneSelectInput
