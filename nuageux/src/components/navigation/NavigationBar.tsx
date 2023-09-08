5e8d6c9a989a782512dcd55dcdefe6f21e27da13
g-ouattara <idrissgouattara@gmail.com>
2023-09-08 16:09:29 +0000

    personalized logout button, onclick event handler

--- nuageux/src/components/navigation/NavigationBar.tsx
+++ nuageux/src/components/navigation/NavigationBar.tsx
@@ -4,9 +4,12 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { RiFileList3Fill } from "react-icons/ri";
 
 
+import { useAuth } from '@/contexts/auth';
 import NavigationBarItem from './NavigationBarItem';
 
 const NavigationBar = () => {
+
+    const { logout } = useAuth();
     return (
         <div
             className="h-14 w-full flex flex-row border-b-2  items-center p-1 border-appPrimary lg:rounded-md justify-between sticky top-0 left-0 bg-appPrimary z-50"
@@ -19,10 +22,16 @@ const NavigationBar = () => {
                     <NavigationBarItem to={"/dashboard"} icon={AiFillPieChart} />
                     <NavigationBarItem to={"/pharmacies"} icon={RiFileList3Fill} />
                     <NavigationBarItem to="/reviews" icon={GiChoice} />
-                    <NavigationBarItem
+                    {/* <NavigationBarItem
                         to="/"
                         icon={IoLogOutOutline}
                         hoverClass={"hover:text-red-500"}
+                        onClick={logout}
+                    /> */}
+                    <IoLogOutOutline
+                        size={42}
+                        onClick={logout}
+                        className={`p-2 text-appGray hover:text-appBlack hover:drop-shadow-md hover:bg-appWhite rounded-md hover:ease-in transition duration-200 hover:text-red-500`}
                     />
 
 
