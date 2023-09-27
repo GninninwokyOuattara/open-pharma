af908d2ef1bfa532c664203be73b4126a7d16c54
g-ouattara <idrissgouattara@gmail.com>
2023-09-27 14:53:38 +0000

    add checkboxes to review table

--- nuageux/src/components/datatables/reviewDatatableColumns.tsx
+++ nuageux/src/components/datatables/reviewDatatableColumns.tsx
@@ -1,10 +1,31 @@
+import { Checkbox } from "@/components/ui/checkbox"
 import { PharmacyBaseData } from "@/types/dataTypes"
 import { ColumnDef } from "@tanstack/react-table"
 import ReviewTableRow from "../reviewTableRow"
 
 
 
+
 export const columns: ColumnDef<PharmacyBaseData>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={table.getIsAllPageRowsSelected()}
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
     {
         accessorKey: "name",
         header: "Name",
