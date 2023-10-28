bbb6d9b8bb7b688fb014657c835fd44f53ff76ad
g-ouattara <idrissgouattara@gmail.com>
2023-10-28 22:59:31 +0000

    instead of two part, one will suffice

--- nuageux/src/constants/endpoint.ts
+++ nuageux/src/constants/endpoint.ts
@@ -1,11 +1,11 @@
 const backendAddress = import.meta.env.VITE_BACKEND_ADDRESS;
-const backendPort = import.meta.env.VITE_BACKEND_PORT;
+// const backendPort = import.meta.env.VITE_BACKEND_PORT;
 
-if (!backendAddress || !backendPort) {
-  throw new Error("VITE_BACKEND_ADDRESS or VITE_BACKEND_PORT is not set");
+if (!backendAddress) {
+  throw new Error("VITE_BACKEND_ADDRESS is not set");
 }
 
-const backendUrl = backendAddress + ":" + backendPort;
+// const backendUrl = backendAddress + ":" + backendPort;
 
-export const administratorEndpoint = backendUrl + "/admin-api";
-export const usersEndpoint = backendUrl + "/api";
+export const administratorEndpoint = backendAddress + "/admin-api";
+export const usersEndpoint = backendAddress + "/api";
