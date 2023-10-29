5c42ecf31ed3cab9e216e54801b447f843b58a50
g-ouattara <idrissgouattara@gmail.com>
2023-10-29 00:18:02 +0000

    trying to have a dynamic debug setup

--- braise/openPharmaRest/openPharmaRest/settings.py
+++ braise/openPharmaRest/openPharmaRest/settings.py
@@ -32,7 +32,9 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-_$gcagc*ak9h)$##7(gg+z#8q$fy8y)g1*lh=j@6o$o^d)#=%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = environ.get('DEBUG')
+DEBUG = True
+if environ.get('DEBUG') == "False":
+    DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
